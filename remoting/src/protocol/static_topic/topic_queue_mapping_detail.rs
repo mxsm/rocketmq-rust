1f30410a9b98fe7f98135c542eadb68a015529ec
mxsm <ljbmxsm@gmail.com>
2023-12-30 23:28:35 +0800

    [ISSUE #11]Update ci.yml (#12)
    * [ISSUE #11]Update ci.yml
    
    * fix ci error
    
    * fix ci
    
    * fix ci error

--- remoting/src/protocol/static_topic/topic_queue_mapping_detail.rs
+++ remoting/src/protocol/static_topic/topic_queue_mapping_detail.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::protocol::static_topic::logic_queue_mapping_item::LogicQueueMappingItem;
 
-#[derive(Debug, Clone, Serialize, Deserialize,Default)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct TopicQueueMappingDetail {
     #[serde(rename = "hostedQueues")]
     hosted_queues: Option<HashMap<i32 /* global id */, Vec<LogicQueueMappingItem>>>,
