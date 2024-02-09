26aa0fc9d30426b88e5b96175fe7b485e23bf472
mxsm <ljbmxsm@gmail.com>
2024-02-09 16:08:40 +0800

    [ISSUE #167]👍Add TopicConfigManager#init method

--- rocketmq-common/src/common/topic.rs
+++ rocketmq-common/src/common/topic.rs
@@ -15,9 +15,39 @@
  * limitations under the License.
  */
 
-pub struct TopicValidator {}
+pub struct TopicValidator;
+
+/*lazy_static! {
+    pub static ref SYSTEM_TOPIC_SET: Mutex<std::collections::HashSet<&'static str>> = {
+        let set = Mutex::new(std::collections::HashSet::new());
+        let mut lock = set.lock().unwrap();
+        lock.insert(TopicValidator::AUTO_CREATE_TOPIC_KEY_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_SCHEDULE_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_BENCHMARK_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_TRANS_HALF_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_TRACE_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_TRANS_OP_HALF_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_TRANS_CHECK_MAX_TIME_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_SELF_TEST_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_OFFSET_MOVED_EVENT);
+        lock.insert(TopicValidator::RMQ_SYS_ROCKSDB_OFFSET_TOPIC);
+        set
+    };
+    pub static ref NOT_ALLOWED_SEND_TOPIC_SET: Mutex<std::collections::HashSet<&'static str>> = {
+        let set = Mutex::new(std::collections::HashSet::new());
+        let mut lock = set.lock().unwrap();
+        lock.insert(TopicValidator::RMQ_SYS_SCHEDULE_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_TRANS_HALF_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_TRANS_OP_HALF_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_TRANS_CHECK_MAX_TIME_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_SELF_TEST_TOPIC);
+        lock.insert(TopicValidator::RMQ_SYS_OFFSET_MOVED_EVENT);
+        set
+    };
+}
+*/
 impl TopicValidator {
-    pub const UTO_CREATE_TOPIC_KEY_TOPIC: &'static str = "TBW102";
+    pub const AUTO_CREATE_TOPIC_KEY_TOPIC: &'static str = "TBW102";
     pub const RMQ_SYS_SCHEDULE_TOPIC: &'static str = "SCHEDULE_TOPIC_XXXX";
     pub const RMQ_SYS_BENCHMARK_TOPIC: &'static str = "BenchmarkTest";
     pub const RMQ_SYS_TRANS_HALF_TOPIC: &'static str = "RMQ_SYS_TRANS_HALF_TOPIC";
