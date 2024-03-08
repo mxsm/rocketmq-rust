c31490a81d60106af26fab3be9adbb4e1a7a60ce
mxsm <ljbmxsm@gmail.com>
2024-03-08 23:07:29 +0800

    [ISSUE #256]🐛Fix rust file link (#257)
    * [ISSUE #256]🐛Fix rust file link
    
    * fix code style

--- rocketmq-broker/src/schedule.rs
+++ rocketmq-broker/src/schedule.rs
@@ -14,4 +14,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+pub(crate) mod delay_offset_serialize_wrapper;
 pub(crate) mod schedule_message_service;
