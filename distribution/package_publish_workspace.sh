65317cfda2b3f827d7cdebbf9429a0d969d3b8df
mxsm <ljbmxsm@gmail.com>
2024-06-05 16:35:23 +0800

    [ISSUE #431]🔨Add rocketmq rust crate package and publish scripts🔨 (#432)

---
+++ distribution/package_publish_workspace.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Save the current directory
+CURRENT_DIR=$(pwd)
+
+# Navigate to the workspace root directory
+cd ..
+
+echo "Starting to package Rust workspace projects..."
+
+PROJECTS=("rocketmq-common" "rocketmq-runtime" "rocketmq-macros" "rocketmq" "rocketmq-filter" "rocketmq-store" "rocketmq-remoting" "rocketmq-cli" "rocketmq-namesrv" "rocketmq-broker")
+
+for PROJECT in "${PROJECTS[@]}"
+do
+    echo "Packaging $PROJECT..."
+    cd $PROJECT
+    cargo package
+    cargo publish
+    if [ $? -ne 0 ]; then
+        echo "$PROJECT packaging failed."
+        cd $CURRENT_DIR
+        exit 1
+    fi
+    cd ..
+done
+
+echo "Finished packaging projects."
+# Return to the original directory
+cd $CURRENT_DIR
