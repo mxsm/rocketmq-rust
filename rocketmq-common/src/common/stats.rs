d2298467e3b4ff85bbe7d46842d993ca2fbea3ef
antcarryele <ljbmxsm@163.com>
2024-06-08 20:01:51 +0800

    [ISSUE #455] ✨Add CallSnapshot struct (#456)
    * [ISSUE #455] ✨Add CallSnapshot struct
    
    * format code

--- rocketmq-common/src/common/stats.rs
+++ rocketmq-common/src/common/stats.rs
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+pub mod call_snapshot;
 pub mod moment_stats_item;
 pub mod stats_snapshot;
 
