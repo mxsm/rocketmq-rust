dc644612a7be041ec46bcaa88d8436515e457ec1
mxsm <ljbmxsm@gmail.com>
2024-06-23 23:44:57 +0800

    [ISSUE #682]🚀Optimize ResponseCode
    (cherry picked from commit dbd78248f3ab4c059ee3e7375cd26c43826bc42a)

--- rocketmq-remoting/src/code/response_code.rs
+++ rocketmq-remoting/src/code/response_code.rs
@@ -109,71 +109,243 @@ pub enum ResponseCode {
     ControllerAlterSyncStateSetFailed = 2013,
     ControllerBrokerIdInvalid = 2014,
 }
-/*impl Into<i32> for ResponseCode {
-    fn into(self) -> i32 {
-        match self {
-            ResponseCode::FlushDiskTimeout => 10,
-            ResponseCode::SlaveNotAvailable => 11,
-            ResponseCode::FlushSlaveTimeout => 12,
-            ResponseCode::MessageIllegal => 13,
-            ResponseCode::ServiceNotAvailable => 14,
-            ResponseCode::VersionNotSupported => 15,
-            ResponseCode::NoPermission => 16,
-            ResponseCode::TopicNotExist => 17,
-            ResponseCode::TopicExistAlready => 18,
-            ResponseCode::PullNotFound => 19,
-            ResponseCode::PullRetryImmediately => 20,
-            ResponseCode::PullOffsetMoved => 21,
-            ResponseCode::QueryNotFound => 22,
-            ResponseCode::SubscriptionParseFailed => 23,
-            ResponseCode::SubscriptionNotExist => 24,
-            ResponseCode::SubscriptionNotLatest => 25,
-            ResponseCode::SubscriptionGroupNotExist => 26,
-            ResponseCode::FilterDataNotExist => 27,
-            ResponseCode::FilterDataNotLatest => 28,
-            ResponseCode::TransactionShouldCommit => 200,
-            ResponseCode::TransactionShouldRollback => 201,
-            ResponseCode::TransactionStateUnknow => 202,
-            ResponseCode::TransactionStateGroupWrong => 203,
-            ResponseCode::NoBuyerId => 204,
-            ResponseCode::NotInCurrentUnit => 205,
-            ResponseCode::ConsumerNotOnline => 206,
-            ResponseCode::ConsumeMsgTimeout => 207,
-            ResponseCode::NoMessage => 208,
-            ResponseCode::PollingFull => 209,
-            ResponseCode::PollingTimeout => 210,
-            ResponseCode::BrokerNotExist => 211,
-            ResponseCode::BrokerDispatchNotComplete => 212,
-            ResponseCode::BroadcastConsumption => 213,
-            ResponseCode::FlowControl => 215,
-            ResponseCode::NotLeaderForQueue => 501,
-            ResponseCode::IllegalOperation => 604,
-            ResponseCode::RpcUnknown => -1000,
-            ResponseCode::RpcAddrIsNull => -1002,
-            ResponseCode::RpcSendToChannelFailed => -1004,
-            ResponseCode::RpcTimeOut => -1006,
-            ResponseCode::GoAway => 1500,
-            ResponseCode::ControllerFencedMasterEpoch => 2000,
-            ResponseCode::ControllerFencedSyncStateSetEpoch => 2001,
-            ResponseCode::ControllerInvalidMaster => 2002,
-            ResponseCode::ControllerInvalidReplicas => 2003,
-            ResponseCode::ControllerMasterNotAvailable => 2004,
-            ResponseCode::ControllerInvalidRequest => 2005,
-            ResponseCode::ControllerBrokerNotAlive => 2006,
-            ResponseCode::ControllerNotLeader => 2007,
-            ResponseCode::ControllerBrokerMetadataNotExist => 2008,
-            ResponseCode::ControllerInvalidCleanBrokerMetadata => 2009,
-            ResponseCode::ControllerBrokerNeedToBeRegistered => 2010,
-            ResponseCode::ControllerMasterStillExist => 2011,
-            ResponseCode::ControllerElectMasterFailed => 2012,
-            ResponseCode::ControllerAlterSyncStateSetFailed => 2013,
-            ResponseCode::ControllerBrokerIdInvalid => 2014,
-        }
-    }
-}*/
 
 impl From<ResponseCode> for i32 {
     fn from(value: ResponseCode) -> Self {
         value as i32
+    }
+}
+
+impl From<i32> for ResponseCode {
+    fn from(value: i32) -> Self {
+        match value {
+            0 => ResponseCode::Success,
+            1 => ResponseCode::SystemError,
+            2 => ResponseCode::SystemBusy,
+            3 => ResponseCode::RequestCodeNotSupported,
+            4 => ResponseCode::TransactionFailed,
+            10 => ResponseCode::FlushDiskTimeout,
+            11 => ResponseCode::SlaveNotAvailable,
+            12 => ResponseCode::FlushSlaveTimeout,
+            13 => ResponseCode::MessageIllegal,
+            14 => ResponseCode::ServiceNotAvailable,
+            15 => ResponseCode::VersionNotSupported,
+            16 => ResponseCode::NoPermission,
+            17 => ResponseCode::TopicNotExist,
+            18 => ResponseCode::TopicExistAlready,
+            19 => ResponseCode::PullNotFound,
+            20 => ResponseCode::PullRetryImmediately,
+            21 => ResponseCode::PullOffsetMoved,
+            22 => ResponseCode::QueryNotFound,
+            23 => ResponseCode::SubscriptionParseFailed,
+            24 => ResponseCode::SubscriptionNotExist,
+            25 => ResponseCode::SubscriptionNotLatest,
+            26 => ResponseCode::SubscriptionGroupNotExist,
+            27 => ResponseCode::FilterDataNotExist,
+            28 => ResponseCode::FilterDataNotLatest,
+            200 => ResponseCode::TransactionShouldCommit,
+            201 => ResponseCode::TransactionShouldRollback,
+            202 => ResponseCode::TransactionStateUnknow,
+            203 => ResponseCode::TransactionStateGroupWrong,
+            204 => ResponseCode::NoBuyerId,
+            205 => ResponseCode::NotInCurrentUnit,
+            206 => ResponseCode::ConsumerNotOnline,
+            207 => ResponseCode::ConsumeMsgTimeout,
+            208 => ResponseCode::NoMessage,
+            209 => ResponseCode::PollingFull,
+            210 => ResponseCode::PollingTimeout,
+            211 => ResponseCode::BrokerNotExist,
+            212 => ResponseCode::BrokerDispatchNotComplete,
+            213 => ResponseCode::BroadcastConsumption,
+            215 => ResponseCode::FlowControl,
+            501 => ResponseCode::NotLeaderForQueue,
+            604 => ResponseCode::IllegalOperation,
+            -1000 => ResponseCode::RpcUnknown,
+            -1002 => ResponseCode::RpcAddrIsNull,
+            -1004 => ResponseCode::RpcSendToChannelFailed,
+            -1006 => ResponseCode::RpcTimeOut,
+            1500 => ResponseCode::GoAway,
+            2000 => ResponseCode::ControllerFencedMasterEpoch,
+            2001 => ResponseCode::ControllerFencedSyncStateSetEpoch,
+            2002 => ResponseCode::ControllerInvalidMaster,
+            2003 => ResponseCode::ControllerInvalidReplicas,
+            2004 => ResponseCode::ControllerMasterNotAvailable,
+            2005 => ResponseCode::ControllerInvalidRequest,
+            2006 => ResponseCode::ControllerBrokerNotAlive,
+            2007 => ResponseCode::ControllerNotLeader,
+            2008 => ResponseCode::ControllerBrokerMetadataNotExist,
+            2009 => ResponseCode::ControllerInvalidCleanBrokerMetadata,
+            2010 => ResponseCode::ControllerBrokerNeedToBeRegistered,
+            2011 => ResponseCode::ControllerMasterStillExist,
+            2012 => ResponseCode::ControllerElectMasterFailed,
+            2013 => ResponseCode::ControllerAlterSyncStateSetFailed,
+            2014 => ResponseCode::ControllerBrokerIdInvalid,
+            _ => ResponseCode::SystemError,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn remoting_sys_response_code_from_i32() {
+        assert_eq!(
+            RemotingSysResponseCode::from(0),
+            RemotingSysResponseCode::Success
+        );
+        assert_eq!(
+            RemotingSysResponseCode::from(1),
+            RemotingSysResponseCode::SystemError
+        );
+        assert_eq!(
+            RemotingSysResponseCode::from(2),
+            RemotingSysResponseCode::SystemBusy
+        );
+        assert_eq!(
+            RemotingSysResponseCode::from(3),
+            RemotingSysResponseCode::RequestCodeNotSupported
+        );
+        assert_eq!(
+            RemotingSysResponseCode::from(4),
+            RemotingSysResponseCode::TransactionFailed
+        );
+        assert_eq!(
+            RemotingSysResponseCode::from(999),
+            RemotingSysResponseCode::SystemError
+        ); // Edge case
+    }
+
+    #[test]
+    fn response_code_from_i32() {
+        assert_eq!(ResponseCode::from(0), ResponseCode::Success);
+        assert_eq!(ResponseCode::from(1), ResponseCode::SystemError);
+        assert_eq!(ResponseCode::from(2), ResponseCode::SystemBusy);
+        assert_eq!(ResponseCode::from(3), ResponseCode::RequestCodeNotSupported);
+        assert_eq!(ResponseCode::from(4), ResponseCode::TransactionFailed);
+        assert_eq!(ResponseCode::from(10), ResponseCode::FlushDiskTimeout);
+        assert_eq!(ResponseCode::from(11), ResponseCode::SlaveNotAvailable);
+        assert_eq!(ResponseCode::from(12), ResponseCode::FlushSlaveTimeout);
+        assert_eq!(ResponseCode::from(13), ResponseCode::MessageIllegal);
+        assert_eq!(ResponseCode::from(14), ResponseCode::ServiceNotAvailable);
+        assert_eq!(ResponseCode::from(15), ResponseCode::VersionNotSupported);
+        assert_eq!(ResponseCode::from(16), ResponseCode::NoPermission);
+        assert_eq!(ResponseCode::from(17), ResponseCode::TopicNotExist);
+        assert_eq!(ResponseCode::from(18), ResponseCode::TopicExistAlready);
+        assert_eq!(ResponseCode::from(19), ResponseCode::PullNotFound);
+        assert_eq!(ResponseCode::from(20), ResponseCode::PullRetryImmediately);
+        assert_eq!(ResponseCode::from(21), ResponseCode::PullOffsetMoved);
+        assert_eq!(ResponseCode::from(22), ResponseCode::QueryNotFound);
+        assert_eq!(
+            ResponseCode::from(23),
+            ResponseCode::SubscriptionParseFailed
+        );
+        assert_eq!(ResponseCode::from(24), ResponseCode::SubscriptionNotExist);
+        assert_eq!(ResponseCode::from(25), ResponseCode::SubscriptionNotLatest);
+        assert_eq!(
+            ResponseCode::from(26),
+            ResponseCode::SubscriptionGroupNotExist
+        );
+        assert_eq!(ResponseCode::from(27), ResponseCode::FilterDataNotExist);
+        assert_eq!(ResponseCode::from(28), ResponseCode::FilterDataNotLatest);
+        assert_eq!(
+            ResponseCode::from(200),
+            ResponseCode::TransactionShouldCommit
+        );
+        assert_eq!(
+            ResponseCode::from(201),
+            ResponseCode::TransactionShouldRollback
+        );
+        assert_eq!(
+            ResponseCode::from(202),
+            ResponseCode::TransactionStateUnknow
+        );
+        assert_eq!(
+            ResponseCode::from(203),
+            ResponseCode::TransactionStateGroupWrong
+        );
+        assert_eq!(ResponseCode::from(204), ResponseCode::NoBuyerId);
+        assert_eq!(ResponseCode::from(205), ResponseCode::NotInCurrentUnit);
+        assert_eq!(ResponseCode::from(206), ResponseCode::ConsumerNotOnline);
+        assert_eq!(ResponseCode::from(207), ResponseCode::ConsumeMsgTimeout);
+        assert_eq!(ResponseCode::from(208), ResponseCode::NoMessage);
+        assert_eq!(ResponseCode::from(209), ResponseCode::PollingFull);
+        assert_eq!(ResponseCode::from(210), ResponseCode::PollingTimeout);
+        assert_eq!(ResponseCode::from(211), ResponseCode::BrokerNotExist);
+        assert_eq!(
+            ResponseCode::from(212),
+            ResponseCode::BrokerDispatchNotComplete
+        );
+        assert_eq!(ResponseCode::from(213), ResponseCode::BroadcastConsumption);
+        assert_eq!(ResponseCode::from(215), ResponseCode::FlowControl);
+        assert_eq!(ResponseCode::from(501), ResponseCode::NotLeaderForQueue);
+        assert_eq!(ResponseCode::from(604), ResponseCode::IllegalOperation);
+        assert_eq!(ResponseCode::from(-1000), ResponseCode::RpcUnknown);
+        assert_eq!(ResponseCode::from(-1002), ResponseCode::RpcAddrIsNull);
+        assert_eq!(
+            ResponseCode::from(-1004),
+            ResponseCode::RpcSendToChannelFailed
+        );
+        assert_eq!(ResponseCode::from(-1006), ResponseCode::RpcTimeOut);
+        assert_eq!(ResponseCode::from(1500), ResponseCode::GoAway);
+        assert_eq!(
+            ResponseCode::from(2000),
+            ResponseCode::ControllerFencedMasterEpoch
+        );
+        assert_eq!(
+            ResponseCode::from(2001),
+            ResponseCode::ControllerFencedSyncStateSetEpoch
+        );
+        assert_eq!(
+            ResponseCode::from(2002),
+            ResponseCode::ControllerInvalidMaster
+        );
+        assert_eq!(
+            ResponseCode::from(2003),
+            ResponseCode::ControllerInvalidReplicas
+        );
+        assert_eq!(
+            ResponseCode::from(2004),
+            ResponseCode::ControllerMasterNotAvailable
+        );
+        assert_eq!(
+            ResponseCode::from(2005),
+            ResponseCode::ControllerInvalidRequest
+        );
+        assert_eq!(
+            ResponseCode::from(2006),
+            ResponseCode::ControllerBrokerNotAlive
+        );
+        assert_eq!(ResponseCode::from(2007), ResponseCode::ControllerNotLeader);
+        assert_eq!(
+            ResponseCode::from(2008),
+            ResponseCode::ControllerBrokerMetadataNotExist
+        );
+        assert_eq!(
+            ResponseCode::from(2009),
+            ResponseCode::ControllerInvalidCleanBrokerMetadata
+        );
+        assert_eq!(
+            ResponseCode::from(2010),
+            ResponseCode::ControllerBrokerNeedToBeRegistered
+        );
+        assert_eq!(
+            ResponseCode::from(2011),
+            ResponseCode::ControllerMasterStillExist
+        );
+        assert_eq!(
+            ResponseCode::from(2012),
+            ResponseCode::ControllerElectMasterFailed
+        );
+        assert_eq!(
+            ResponseCode::from(2013),
+            ResponseCode::ControllerAlterSyncStateSetFailed
+        );
+        assert_eq!(
+            ResponseCode::from(2014),
+            ResponseCode::ControllerBrokerIdInvalid
+        );
+        assert_eq!(ResponseCode::from(9999), ResponseCode::SystemError); // Edge case
     }
 }
