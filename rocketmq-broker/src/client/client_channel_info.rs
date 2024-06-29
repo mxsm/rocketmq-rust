df4f2b8d4a85faa858c2cb2fecb6a6912d79f333
alax-cat <henyang2024@163.com>
2024-06-29 01:37:58 +0000

    [ISSUE #715]✅Add test case for ClientChannelInfo

--- rocketmq-broker/src/client/client_channel_info.rs
+++ rocketmq-broker/src/client/client_channel_info.rs
@@ -78,3 +78,55 @@ impl ClientChannelInfo {
         self.channel = channel;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rocketmq_remoting::net::channel::Channel;
+    use rocketmq_remoting::protocol::LanguageCode;
+
+    use super::*;
+
+    #[test]
+    fn client_channel_info_new() {
+        let channel = Channel::new(
+            "127.0.0.1:8080".parse().unwrap(),
+            "127.0.0.1:8080".parse().unwrap(),
+        );
+        let client_info = ClientChannelInfo::new(
+            channel.clone(),
+            "client1".to_string(),
+            LanguageCode::JAVA,
+            1,
+        );
+        assert_eq!(client_info.client_id(), "client1");
+        assert_eq!(client_info.language(), LanguageCode::JAVA);
+        assert_eq!(client_info.version(), 1);
+        assert_eq!(client_info.channel(), &channel);
+    }
+
+    #[test]
+    fn client_channel_info_setters() {
+        let channel = Channel::new(
+            "127.0.0.1:8080".parse().unwrap(),
+            "127.0.0.1:8080".parse().unwrap(),
+        );
+        let mut client_info = ClientChannelInfo::new(
+            channel.clone(),
+            "client1".to_string(),
+            LanguageCode::JAVA,
+            1,
+        );
+        client_info.set_client_id("client2".to_string());
+        client_info.set_language(LanguageCode::CPP);
+        client_info.set_version(2);
+        let new_channel = Channel::new(
+            "127.0.0.1:8081".parse().unwrap(),
+            "127.0.0.1:8080".parse().unwrap(),
+        );
+        client_info.set_channel(new_channel.clone());
+        assert_eq!(client_info.client_id(), "client2");
+        assert_eq!(client_info.language(), LanguageCode::CPP);
+        assert_eq!(client_info.version(), 2);
+        assert_eq!(client_info.channel(), &new_channel);
+    }
+}
