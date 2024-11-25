c53f3765ae500e8aa4e6ea704505c2820a6c939a
mxsm <mxsm@apache.org>
2024-11-25 08:43:39 +0000

    [ISSUE #1303]🚀Add QueryAssignmentRequestBody and QueryAssignmentResponseBody

--- rocketmq-remoting/src/protocol/body/query_assignment_response_body.rs
+++ rocketmq-remoting/src/protocol/body/query_assignment_response_body.rs
@@ -14,26 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use std::collections::HashSet;
 
-pub mod broker_body;
-pub mod consumer_running_info;
-pub mod create_topic_list_request_body;
-pub mod get_consumer_listby_group_response_body;
+use rocketmq_common::common::message::message_queue_assignment::MessageQueueAssignment;
+use serde::Deserialize;
+use serde::Serialize;
 
-pub mod consumer_connection;
-
-pub mod check_client_request_body;
-pub mod cm_result;
-pub mod connection;
-pub mod consume_message_directly_result;
-pub mod group_list;
-pub mod kv_table;
-pub mod pop_process_queue_info;
-pub mod process_queue_info;
-pub mod query_assignment_request_body;
-pub mod query_assignment_response_body;
-pub mod request;
-pub mod response;
-pub mod topic;
-pub mod topic_info_wrapper;
-pub mod unlock_batch_request_body;
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct QueryAssignmentResponseBody {
+    pub message_queue_assignments: HashSet<MessageQueueAssignment>,
+}
