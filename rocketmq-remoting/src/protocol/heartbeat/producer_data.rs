5cbca709f0a7f98c68f92c9a78239d89b8941b0b
what-design <3852355058@qq.com>
2024-11-26 22:28:24 +0800

    [ISSUE #1338]🧪Add Unit test for ProducerData (#1339)

--- rocketmq-remoting/src/protocol/heartbeat/producer_data.rs
+++ rocketmq-remoting/src/protocol/heartbeat/producer_data.rs
@@ -23,3 +23,79 @@ use serde::Serialize;
 pub struct ProducerData {
     pub group_name: CheetahString,
 }
+
+#[cfg(test)]
+mod tests {
+    use serde_json;
+
+    use super::*;
+
+    #[test]
+    fn producer_data_default_values() {
+        let producer_data = ProducerData::default();
+        assert_eq!(producer_data.group_name, CheetahString::new());
+    }
+
+    #[test]
+    fn producer_data_equality() {
+        let producer_data1 = ProducerData {
+            group_name: CheetahString::from("group1"),
+        };
+
+        let producer_data2 = ProducerData {
+            group_name: CheetahString::from("group1"),
+        };
+
+        assert_eq!(producer_data1, producer_data2);
+    }
+
+    #[test]
+    fn producer_data_inequality() {
+        let producer_data1 = ProducerData {
+            group_name: CheetahString::from("group1"),
+        };
+
+        let producer_data2 = ProducerData {
+            group_name: CheetahString::from("group2"),
+        };
+
+        assert_ne!(producer_data1, producer_data2);
+    }
+
+    #[test]
+    fn serialize_producer_data() {
+        let producer_data = ProducerData {
+            group_name: CheetahString::from("group1"),
+        };
+        let serialized = serde_json::to_string(&producer_data).unwrap();
+        assert_eq!(serialized, r#"{"groupName":"group1"}"#);
+    }
+
+    #[test]
+    fn deserialize_producer_data() {
+        let json = r#"{"groupName":"group1"}"#;
+        let deserialized: ProducerData = serde_json::from_str(json).unwrap();
+        assert_eq!(deserialized.group_name, CheetahString::from("group1"));
+    }
+
+    #[test]
+    fn producer_data_hash() {
+        use std::collections::hash_map::DefaultHasher;
+        use std::hash::Hash;
+        use std::hash::Hasher;
+
+        let producer_data = ProducerData {
+            group_name: CheetahString::from("group1"),
+        };
+
+        let mut hasher = DefaultHasher::new();
+        producer_data.hash(&mut hasher);
+        let hash1 = hasher.finish();
+
+        let mut hasher = DefaultHasher::new();
+        producer_data.hash(&mut hasher);
+        let hash2 = hasher.finish();
+
+        assert_eq!(hash1, hash2);
+    }
+}
