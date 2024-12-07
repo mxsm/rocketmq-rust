36669f16e8ba3e6b19cca80ac2f2ab4564825499
mxsm <mxsm@apache.org>
2024-12-07 20:15:30 +0800

    [ISSUE #1642]♻️Refactor AckMessageProcessor#process_request method signature🔥 (#1643)

--- rocketmq-broker/src/processor/ack_message_processor.rs
+++ rocketmq-broker/src/processor/ack_message_processor.rs
@@ -14,6 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use rocketmq_remoting::code::request_code::RequestCode;
+use rocketmq_remoting::net::channel::Channel;
 use rocketmq_remoting::protocol::remoting_command::RemotingCommand;
 use rocketmq_remoting::runtime::connection_handler_context::ConnectionHandlerContext;
 
@@ -21,11 +23,13 @@ use rocketmq_remoting::runtime::connection_handler_context::ConnectionHandlerCon
 pub struct AckMessageProcessor {}
 
 impl AckMessageProcessor {
-    fn process_request(
-        &self,
+    pub async fn process_request(
+        &mut self,
+        _channel: Channel,
         _ctx: ConnectionHandlerContext,
+        _request_code: RequestCode,
         _request: RemotingCommand,
-    ) -> RemotingCommand {
-        todo!()
+    ) -> crate::Result<Option<RemotingCommand>> {
+        unimplemented!("AckMessageProcessor process_request")
     }
 }
