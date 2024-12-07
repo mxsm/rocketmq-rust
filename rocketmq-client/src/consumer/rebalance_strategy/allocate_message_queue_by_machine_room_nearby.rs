09958fc7900b6d29719bf2e1669c307783d3f8c4
Sainath Singineedi <sainathsingineedi2222@gmail.com>
2024-12-07 20:35:36 +0530

    Coderabbit suggestions v1

--- rocketmq-client/src/consumer/rebalance_strategy/allocate_message_queue_by_machine_room_nearby.rs
+++ rocketmq-client/src/consumer/rebalance_strategy/allocate_message_queue_by_machine_room_nearby.rs
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 use std::collections::BTreeMap;
 
 use cheetah_string::CheetahString;
@@ -112,6 +128,7 @@ mod tests {
     use std::collections::BTreeSet;
 
     use rand::Rng;
+    use rand::SeedableRng;
 
     use super::*;
     use crate::consumer::rebalance_strategy::allocate_message_queue_averagely_by_circle::AllocateMessageQueueAveragelyByCircle;
@@ -336,7 +353,7 @@ mod tests {
 
     #[test]
     fn test_run_10_random_case() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rngs::StdRng::seed_from_u64(42);
         for _ in 0..10 {
             let consumer_size = rng.gen_range(1..=200);
             let queue_size = rng.gen_range(1..=100);
