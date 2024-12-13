1d742164f840a982b5ec1b05ac193ec4c52d3c32
mxsm <mxsm@apache.org>
2024-12-13 18:37:28 +0800

    [ISSUE #1746]🍻Implement netty Timeout for rust trait🤡 (#1751)

--- rocketmq/src/netty_rust/timeout.rs
+++ rocketmq/src/netty_rust/timeout.rs
@@ -14,7 +14,47 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use std::sync::Arc;
+
+use crate::netty_rust::timer_task::TimerTask;
+use crate::Timer;
 
 ///convert from netty [Timeout](https://github.com/netty/netty/blob/4.1/common/src/main/java/io/netty/util/Timeout.java)
+/// A trait representing a timeout that can be managed by a timer.
 #[allow(dead_code)]
-pub trait Timeout {}
+pub trait Timeout {
+    /// Returns the timer associated with this timeout.
+    ///
+    /// # Returns
+    ///
+    /// An `Arc` containing the timer.
+    fn timer(&self) -> Arc<dyn Timer>;
+
+    /// Returns the task associated with this timeout.
+    ///
+    /// # Returns
+    ///
+    /// An `Arc` containing the timer task.
+    fn task(&self) -> Arc<dyn TimerTask>;
+
+    /// Checks if the timeout has expired.
+    ///
+    /// # Returns
+    ///
+    /// `true` if the timeout has expired, `false` otherwise.
+    fn is_expired(&self) -> bool;
+
+    /// Checks if the timeout has been cancelled.
+    ///
+    /// # Returns
+    ///
+    /// `true` if the timeout has been cancelled, `false` otherwise.
+    fn is_cancelled(&self) -> bool;
+
+    /// Cancels the timeout.
+    ///
+    /// # Returns
+    ///
+    /// `true` if the timeout was successfully cancelled, `false` otherwise.
+    fn cancel(&self) -> bool;
+}
