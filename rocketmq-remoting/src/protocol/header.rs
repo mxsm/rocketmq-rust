996ebdfbaef6495693f1cb1fa989833952d0faf4
mxsm <mxsm@apache.org>
2024-12-17 16:43:45 +0800

    [ISSUE #1825]🧑‍💻Add AckMessageRequestHeader for rust (#1826)

--- rocketmq-remoting/src/protocol/header.rs
+++ rocketmq-remoting/src/protocol/header.rs
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+pub mod ack_message_request_header;
 pub mod broker;
 pub mod change_invisible_time_request_header;
 pub mod change_invisible_time_response_header;
---
+++ rocketmq-remoting/src/protocol/header/ack_message_request_header.rs
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+use cheetah_string::CheetahString;
+use rocketmq_macros::RequestHeaderCodec;
+use serde::Deserialize;
+use serde::Serialize;
+
+use crate::rpc::topic_request_header::TopicRequestHeader;
+
+/// Represents the request header for acknowledging a message.
+#[derive(Debug, Serialize, Deserialize, Clone, RequestHeaderCodec)]
+pub struct AckMessageRequestHeader {
+    /// Consumer group name (required)
+    #[serde(rename = "consumerGroup")]
+    #[required]
+    pub consumer_group: CheetahString,
+
+    /// Topic name (required)
+    #[serde(rename = "topic")]
+    #[required]
+    pub topic: CheetahString,
+
+    /// Queue ID (required)
+    #[serde(rename = "queueId")]
+    #[required]
+    pub queue_id: i32,
+
+    /// Extra information (required)
+    #[serde(rename = "extraInfo")]
+    #[required]
+    pub extra_info: CheetahString,
+
+    /// Offset (required)
+    #[serde(rename = "offset")]
+    #[required]
+    pub offset: i64,
+
+    #[serde(flatten)]
+    pub topic_request_header: Option<TopicRequestHeader>,
+}
+
+#[cfg(test)]
+mod tests {
+    use cheetah_string::CheetahString;
+    use serde_json;
+
+    use super::*;
+
+    #[test]
+    fn serialize_ack_message_request_header() {
+        let header = AckMessageRequestHeader {
+            consumer_group: CheetahString::from("test_group"),
+            topic: CheetahString::from("test_topic"),
+            queue_id: 1,
+            extra_info: CheetahString::from("extra_info"),
+            offset: 12345,
+            topic_request_header: None,
+        };
+        let json = serde_json::to_string(&header).unwrap();
+        let expected = r#"{"consumerGroup":"test_group","topic":"test_topic","queueId":1,"extraInfo":"extra_info","offset":12345}"#;
+        assert_eq!(json, expected);
+    }
+
+    #[test]
+    fn deserialize_ack_message_request_header() {
+        let json = r#"{"consumerGroup":"test_group","topic":"test_topic","queueId":1,"extraInfo":"extra_info","offset":12345}"#;
+        let header: AckMessageRequestHeader = serde_json::from_str(json).unwrap();
+        assert_eq!(header.consumer_group, CheetahString::from("test_group"));
+        assert_eq!(header.topic, CheetahString::from("test_topic"));
+        assert_eq!(header.queue_id, 1);
+        assert_eq!(header.extra_info, CheetahString::from("extra_info"));
+        assert_eq!(header.offset, 12345);
+        assert!(!header.topic_request_header.is_none());
+    }
+
+    #[test]
+    fn deserialize_ack_message_request_header_with_topic_request_header() {
+        let json = r#"{"consumerGroup":"test_group","topic":"test_topic","queueId":1,"extraInfo":"extra_info","offset":12345,"topicRequestHeader":{"someField":"someValue"}}"#;
+        let header: AckMessageRequestHeader = serde_json::from_str(json).unwrap();
+        assert_eq!(header.consumer_group, CheetahString::from("test_group"));
+        assert_eq!(header.topic, CheetahString::from("test_topic"));
+        assert_eq!(header.queue_id, 1);
+        assert_eq!(header.extra_info, CheetahString::from("extra_info"));
+        assert_eq!(header.offset, 12345);
+        assert!(header.topic_request_header.is_some());
+    }
+
+    #[test]
+    fn serialize_ack_message_request_header_with_topic_request_header() {
+        let header = AckMessageRequestHeader {
+            consumer_group: CheetahString::from("test_group"),
+            topic: CheetahString::from("test_topic"),
+            queue_id: 1,
+            extra_info: CheetahString::from("extra_info"),
+            offset: 12345,
+            topic_request_header: None,
+        };
+        let json = serde_json::to_string(&header).unwrap();
+        let expected = r#"{"consumerGroup":"test_group","topic":"test_topic","queueId":1,"extraInfo":"extra_info","offset":12345}"#;
+        assert_eq!(json, expected);
+    }
+}
