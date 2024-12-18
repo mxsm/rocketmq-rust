4ce5c8757d9661e6552006d05ca2be44fc28fe00
mxsm <ljbmxsm@gmail.com>
2024-12-18 10:55:23 +0800

    [ISSUE #1840]Add PopReviveService struce for rust

--- rocketmq-broker/src/processor/processor_service/pop_revive_service.rs
+++ rocketmq-broker/src/processor/processor_service/pop_revive_service.rs
@@ -14,5 +14,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-pub(crate) mod pop_buffer_merge_service;
-pub(super) mod pop_revive_service;
+pub struct PopReviveService;
