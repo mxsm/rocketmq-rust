d432b2c4877ea63b6a7e5acfbf25c9d52a609198
mxsm <mxsm@apache.org>
2024-12-19 02:12:56 +0000

    [ISSUE #1858]⚡️AckMessage trait add get and set methods

--- rocketmq-store/src/pop.rs
+++ rocketmq-store/src/pop.rs
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+use cheetah_string::CheetahString;
 
 pub mod ack_msg;
 pub mod batch_ack_msg;
@@ -21,6 +22,27 @@ pub mod pop_check_point;
 
 /// A trait representing an acknowledgment message that can be converted to and from `Any`.
 pub trait AckMessage {
+    fn ack_offset(&self) -> i64;
+    fn set_ack_offset(&mut self, ack_offset: i64);
+
+    fn start_offset(&self) -> i64;
+    fn set_start_offset(&mut self, start_offset: i64);
+
+    fn consumer_group(&self) -> &CheetahString;
+    fn set_consumer_group(&mut self, consumer_group: CheetahString);
+
+    fn topic(&self) -> &CheetahString;
+    fn set_topic(&mut self, topic: CheetahString);
+
+    fn queue_id(&self) -> i32;
+    fn set_queue_id(&mut self, queue_id: i32);
+
+    fn pop_time(&self) -> i64;
+    fn set_pop_time(&mut self, pop_time: i64);
+
+    fn broker_name(&self) -> &CheetahString;
+    fn set_broker_name(&mut self, broker_name: CheetahString);
+
     /// Converts the acknowledgment message to a reference of type `Any`.
     ///
     /// # Returns
--- rocketmq-store/src/pop/ack_msg.rs
+++ rocketmq-store/src/pop/ack_msg.rs
@@ -47,6 +47,62 @@ pub struct AckMsg {
 }
 
 impl AckMessage for AckMsg {
+    fn ack_offset(&self) -> i64 {
+        self.ack_offset
+    }
+
+    fn set_ack_offset(&mut self, ack_offset: i64) {
+        self.ack_offset = ack_offset;
+    }
+
+    fn start_offset(&self) -> i64 {
+        self.start_offset
+    }
+
+    fn set_start_offset(&mut self, start_offset: i64) {
+        self.start_offset = start_offset;
+    }
+
+    fn consumer_group(&self) -> &CheetahString {
+        &self.consumer_group
+    }
+
+    fn set_consumer_group(&mut self, consumer_group: CheetahString) {
+        self.consumer_group = consumer_group;
+    }
+
+    fn topic(&self) -> &CheetahString {
+        &self.topic
+    }
+
+    fn set_topic(&mut self, topic: CheetahString) {
+        self.topic = topic;
+    }
+
+    fn queue_id(&self) -> i32 {
+        self.queue_id
+    }
+
+    fn set_queue_id(&mut self, queue_id: i32) {
+        self.queue_id = queue_id;
+    }
+
+    fn pop_time(&self) -> i64 {
+        self.pop_time
+    }
+
+    fn set_pop_time(&mut self, pop_time: i64) {
+        self.pop_time = pop_time;
+    }
+
+    fn broker_name(&self) -> &CheetahString {
+        &self.broker_name
+    }
+
+    fn set_broker_name(&mut self, broker_name: CheetahString) {
+        self.broker_name = broker_name;
+    }
+
     fn as_any(&self) -> &dyn std::any::Any {
         self
     }
