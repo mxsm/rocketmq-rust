5988865a6dabfe16d25ad19180f85e1cd9ae5fd6
mxsm <mxsm@apache.org>
2025-02-14 15:48:28 +0800

    [ISSUE #2542]🔨Update package_publish_workspace.sh🔥 (#2543)

--- distribution/package_publish_workspace.sh
+++ distribution/package_publish_workspace.sh
@@ -8,7 +8,22 @@ cd ..
 
 echo "Starting to package Rust workspace projects..."
 
-PROJECTS=("rocketmq-common" "rocketmq-runtime" "rocketmq-macros" "rocketmq" "rocketmq-filter" "rocketmq-store" "rocketmq-remoting" "rocketmq-cli" "rocketmq-example" "rocketmq-client" "rocketmq-namesrv" "rocketmq-broker","rocketmq-tools")
+PROJECTS=(
+    "rocketmq-common"
+    "rocketmq-runtime"
+    "rocketmq-macros"
+    "rocketmq"
+    "rocketmq-filter"
+    "rocketmq-store"
+    "rocketmq-remoting"
+    "rocketmq-cli"
+    "rocketmq-example"
+    "rocketmq-client"
+    "rocketmq-namesrv"
+    "rocketmq-broker"
+    "rocketmq-tools"
+    "rocketmq-tui"
+)
 
 for PROJECT in "${PROJECTS[@]}"
 do
