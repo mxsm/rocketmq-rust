5845edb3240b30dc7198fb12d96ec7661d5a6513
decadezzz <115476365+decadezzz@users.noreply.github.com>
2025-02-17 09:15:29 +0800

    Add QueueTimeSpan and MessageTrack (#2560)

--- rocketmq-remoting/src/protocol/body/queue_time_span.rs
+++ rocketmq-remoting/src/protocol/body/queue_time_span.rs
@@ -15,15 +15,69 @@
  * limitations under the License.
  */
 use rocketmq_common::common::message::message_queue::MessageQueue;
+use rocketmq_common::UtilAll::time_millis_to_human_string2;
 use serde::Deserialize;
 use serde::Serialize;
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
-#[serde(rename_all = "camelCase")]
 pub struct QueueTimeSpan {
     pub message_queue: Option<MessageQueue>,
     pub min_time_stamp: i64,
     pub max_time_stamp: i64,
     pub consume_time_stamp: i64,
     pub delay_time: i64,
+}
+
+impl QueueTimeSpan {
+    pub fn get_message_queue(&self) -> Option<MessageQueue> {
+        self.message_queue.clone()
+    }
+
+    pub fn set_message_queue(&mut self, message_queue: MessageQueue) {
+        self.message_queue = Some(message_queue);
+    }
+
+    pub fn get_min_time_stamp(&self) -> i64 {
+        self.min_time_stamp
+    }
+
+    pub fn set_min_time_stamp(&mut self, min_time_stamp: i64) {
+        self.min_time_stamp = min_time_stamp;
+    }
+
+    pub fn get_max_time_stamp(&self) -> i64 {
+        self.max_time_stamp
+    }
+
+    pub fn set_max_time_stamp(&mut self, max_time_stamp: i64) {
+        self.max_time_stamp = max_time_stamp;
+    }
+
+    pub fn get_consume_time_stamp(&self) -> i64 {
+        self.consume_time_stamp
+    }
+
+    pub fn set_consume_time_stamp(&mut self, consume_time_stamp: i64) {
+        self.consume_time_stamp = consume_time_stamp;
+    }
+
+    pub fn get_delay_time(&self) -> i64 {
+        self.delay_time
+    }
+
+    pub fn set_delay_time(&mut self, delay_time: i64) {
+        self.delay_time = delay_time;
+    }
+
+    pub fn get_min_time_stamp_str(&self) -> String {
+        time_millis_to_human_string2(self.min_time_stamp)
+    }
+
+    pub fn get_max_time_stamp_str(&self) -> String {
+        time_millis_to_human_string2(self.max_time_stamp)
+    }
+
+    pub fn get_consume_time_stamp_str(&self) -> String {
+        time_millis_to_human_string2(self.consume_time_stamp)
+    }
 }
