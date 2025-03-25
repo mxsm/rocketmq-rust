ce2a305a98de73afc3e9333bef169e2d91475dc9
mxsm <mxsm@apache.org>
2025-03-25 09:54:32 +0000

    code format

--- rocketmq-broker/src/hook/schedule_message_hook.rs
+++ rocketmq-broker/src/hook/schedule_message_hook.rs
@@ -15,24 +15,28 @@
  * limitations under the License.
  */
 
- use rocketmq_common::common::message::message_ext_broker_inner::MessageExtBrokerInner;
- use rocketmq_rust::ArcMut;
- use rocketmq_store::base::message_result::PutMessageResult;
- use rocketmq_store::base::message_store::MessageStore;
- use rocketmq_store::hook::put_message_hook::PutMessageHook;
- use crate::broker_runtime::BrokerRuntimeInner;
- use crate::util::hook_utils::HookUtils;
- 
- pub struct ScheduleMessageHook<MS> {
-     broker_runtime_inner: ArcMut<BrokerRuntimeInner<MS>>,
- }
- 
- impl<MS:MessageStore> PutMessageHook for ScheduleMessageHook<MS> {
-     fn hook_name(&self) -> String {
-         "ScheduleMessageHook".to_string()
-     }
- 
-     fn execute_before_put_message(&self, msg: &mut MessageExtBrokerInner) -> Option<PutMessageResult> {
-         HookUtils::handle_schedule_message(&self.broker_runtime_inner, msg)
-     }
- }
+use rocketmq_common::common::message::message_ext_broker_inner::MessageExtBrokerInner;
+use rocketmq_rust::ArcMut;
+use rocketmq_store::base::message_result::PutMessageResult;
+use rocketmq_store::base::message_store::MessageStore;
+use rocketmq_store::hook::put_message_hook::PutMessageHook;
+
+use crate::broker_runtime::BrokerRuntimeInner;
+use crate::util::hook_utils::HookUtils;
+
+pub struct ScheduleMessageHook<MS> {
+    broker_runtime_inner: ArcMut<BrokerRuntimeInner<MS>>,
+}
+
+impl<MS: MessageStore> PutMessageHook for ScheduleMessageHook<MS> {
+    fn hook_name(&self) -> String {
+        "ScheduleMessageHook".to_string()
+    }
+
+    fn execute_before_put_message(
+        &self,
+        msg: &mut MessageExtBrokerInner,
+    ) -> Option<PutMessageResult> {
+        HookUtils::handle_schedule_message(&self.broker_runtime_inner, msg)
+    }
+}
