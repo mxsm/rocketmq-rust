c4ebd2020846b3d535d0136ab35c401e5fa713f1
mxsm <mxsm@apache.org>
2025-03-29 04:48:29 +0000

    [ISSUE #2890]🐛Key hash code abs may with panic

--- rocketmq-store/src/index/index_file.rs
+++ rocketmq-store/src/index/index_file.rs
@@ -216,12 +216,11 @@ impl IndexFile {
     }
 
     pub fn index_key_hash_method(&self, key: &str) -> i32 {
-        let key_hash = JavaStringHasher::hash_str(key);
-        let key_hash_positive = key_hash.abs();
-        if key_hash_positive < 0 {
+        let hash_code = JavaStringHasher::hash_str(key);
+        if hash_code == i32::MIN {
             0
         } else {
-            key_hash_positive
+            hash_code.abs()
         }
     }
 
