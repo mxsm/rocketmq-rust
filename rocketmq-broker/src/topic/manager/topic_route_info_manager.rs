f8d1ea67b1107cbcc34a8d34b82bb5054c6aea35
mxsm <mxsm@apache.org>
2025-04-06 22:53:36 +0800

    [ISSUE #3014]⚡️Simplify TopicRouteInfoManager#get_topic_subscribe_info💫

--- rocketmq-broker/src/topic/manager/topic_route_info_manager.rs
+++ rocketmq-broker/src/topic/manager/topic_route_info_manager.rs
@@ -314,7 +314,7 @@ impl<MS: MessageStore> TopicRouteInfoManager<MS> {
         topic: &CheetahString,
     ) -> Option<HashSet<MessageQueue>> {
         let mut queues = self.topic_subscribe_info_table.get(topic).cloned();
-        if queues.is_none() || queues.as_ref().unwrap().is_empty() {
+        if queues.as_ref().map_or(true, |q| q.is_empty()) {
             self.update_topic_route_info_from_name_server_ext(topic, false, true)
                 .await;
             queues = self.topic_subscribe_info_table.get(topic).cloned();
