2639d7bd637332d88d76b937a8c31dd0a070e771
tonidong <tonidong@tencent.com>
2025-06-19 22:20:49 +0800

    [ISSUE #3390] ♻️Refactor GetConsumerListByGroupRequestHeader with derive marco RequestHeaderCodec

--- rocketmq-remoting/src/protocol/header/get_consumer_listby_group_request_header.rs
+++ rocketmq-remoting/src/protocol/header/get_consumer_listby_group_request_header.rs
@@ -14,62 +14,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-use std::collections::HashMap;
-
+use rocketmq_macros::RequestHeaderCodec; 
 use cheetah_string::CheetahString;
 use serde::Deserialize;
 use serde::Serialize;
 
-use crate::protocol::command_custom_header::CommandCustomHeader;
-use crate::protocol::command_custom_header::FromMap;
 use crate::rpc::rpc_request_header::RpcRequestHeader;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, RequestHeaderCodec)]
 #[serde(rename_all = "camelCase")]
 pub struct GetConsumerListByGroupRequestHeader {
+    #[required]
     pub consumer_group: CheetahString,
+
     #[serde(flatten)]
     pub rpc: Option<RpcRequestHeader>,
 }
 
-impl GetConsumerListByGroupRequestHeader {
-    pub const CONSUMER_GROUP: &'static str = "consumerGroup";
-}
-
-impl CommandCustomHeader for GetConsumerListByGroupRequestHeader {
-    fn to_map(&self) -> Option<HashMap<CheetahString, CheetahString>> {
-        let mut map = HashMap::new();
-        map.insert(
-            CheetahString::from_static_str(Self::CONSUMER_GROUP),
-            self.consumer_group.clone(),
-        );
-        if let Some(ref rpc) = self.rpc {
-            if let Some(rpc_map) = rpc.to_map() {
-                map.extend(rpc_map);
-            }
-        }
-        Some(map)
-    }
-}
-impl FromMap for GetConsumerListByGroupRequestHeader {
-    type Error = rocketmq_error::RocketmqError;
-
-    type Target = Self;
-
-    fn from(map: &HashMap<CheetahString, CheetahString>) -> Result<Self::Target, Self::Error> {
-        Ok(GetConsumerListByGroupRequestHeader {
-            consumer_group: map
-                .get(&CheetahString::from_static_str(Self::CONSUMER_GROUP))
-                .cloned()
-                .unwrap_or_default(),
-            rpc: Some(<RpcRequestHeader as FromMap>::from(map)?),
-        })
-    }
-}
 
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
+    use crate::protocol::command_custom_header::CommandCustomHeader;
+    use crate::protocol::command_custom_header::FromMap;
 
     use super::*;
 
@@ -80,7 +47,7 @@ mod tests {
             rpc: None,
         };
 
-        let map = header.to_map().unwrap();
+        let map: HashMap<CheetahString, CheetahString> = header.to_map().unwrap();
         assert_eq!(
             map.get(GetConsumerListByGroupRequestHeader::CONSUMER_GROUP),
             Some(&"test_group".into())
