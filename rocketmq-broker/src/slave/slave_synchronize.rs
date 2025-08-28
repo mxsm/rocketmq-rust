dd2cb49aca4e19bc746be0d4bdf7d7a5a42c0620
mxsm <mxsm@apache.org>
2025-08-28 22:55:50 +0800

    [ISSUE #3869]♻️Make SlaveSynchronize generic and optional; integrate safely into BrokerRuntime (#3870)

--- rocketmq-broker/src/slave/slave_synchronize.rs
+++ rocketmq-broker/src/slave/slave_synchronize.rs
@@ -14,11 +14,48 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+use rocketmq_rust::ArcMut;
+use rocketmq_store::base::message_store::MessageStore;
 use tracing::error;
+use tracing::info;
 
-pub(crate) struct SlaveSynchronize;
+use crate::broker_runtime::BrokerRuntimeInner;
 
-impl SlaveSynchronize {
+pub(crate) struct SlaveSynchronize<MS: MessageStore> {
+    broker_runtime_inner: ArcMut<BrokerRuntimeInner<MS>>,
+    master_addr: Option<String>,
+}
+
+impl<MS> SlaveSynchronize<MS>
+where
+    MS: MessageStore,
+{
+    pub fn new(broker_runtime_inner: ArcMut<BrokerRuntimeInner<MS>>) -> Self {
+        Self {
+            broker_runtime_inner,
+            master_addr: None,
+        }
+    }
+
+    pub fn master_addr(&self) -> Option<&String> {
+        self.master_addr.as_ref()
+    }
+
+    pub fn set_master_addr(&mut self, addr: String) {
+        if let Some(current_addr) = &self.master_addr {
+            if current_addr == &addr {
+                return;
+            }
+        }
+        info!(
+            "Update master address from {} to {}",
+            self.master_addr.as_deref().unwrap_or("None"),
+            addr
+        );
+        self.master_addr = Some(addr);
+    }
+
     pub async fn sync_all(&self) {
         error!("SlaveSynchronize::sync_all is not implemented yet");
     }
