0649d669b81b836146c4f0fa04e3db6579b58c24
mxsm <mxsm@apache.org>
2025-12-05 06:40:27 +0000

    fix ci error

--- rocketmq-store/src/log_file/mapped_file/default_mapped_file_impl.rs
+++ rocketmq-store/src/log_file/mapped_file/default_mapped_file_impl.rs
@@ -987,7 +987,7 @@ impl MappedFile for DefaultMappedFile {
                 size,
                 bytes: Some(bytes),
                 is_in_cache: true,
-                ..Default::default()
+                mapped_file: None,
             })
         } else {
             None
