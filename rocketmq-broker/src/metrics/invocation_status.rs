9d283a4ea7b06c9f41a48a77acf4b80b0ce5500d
mxsm <mxsm@apache.org>
2025-12-14 11:49:20 +0800

    [ISSUE #4634]🚀Add InvocationStatus enum for metrics tracking (#4635)

---
+++ rocketmq-broker/src/metrics/invocation_status.rs
@@ -0,0 +1,61 @@
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+
+use std::fmt::Display;
+
+use serde::Deserialize;
+use serde::Serialize;
+
+/// Invocation status for metrics tracking
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub enum InvocationStatus {
+    Success,
+    Failure,
+}
+
+impl InvocationStatus {
+    /// Get the name of the invocation status
+    pub fn get_name(&self) -> &'static str {
+        match self {
+            InvocationStatus::Success => "success",
+            InvocationStatus::Failure => "failure",
+        }
+    }
+}
+
+impl Display for InvocationStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.get_name())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_invocation_status_name() {
+        assert_eq!(InvocationStatus::Success.get_name(), "success");
+        assert_eq!(InvocationStatus::Failure.get_name(), "failure");
+    }
+
+    #[test]
+    fn test_invocation_status_display() {
+        assert_eq!(format!("{}", InvocationStatus::Success), "success");
+        assert_eq!(format!("{}", InvocationStatus::Failure), "failure");
+    }
+}
