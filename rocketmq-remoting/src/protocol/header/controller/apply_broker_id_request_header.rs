c670a0ae31d1fc045b35092b06c5350dc54f060a
mxsm <mxsm@apache.org>
2025-12-18 07:19:14 +0000

    [ISSUE #4760] 🚀Add ApplyBrokerIdRequestHeader struct

---
+++ rocketmq-remoting/src/protocol/header/controller/apply_broker_id_request_header.rs
@@ -0,0 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use cheetah_string::CheetahString;
+use rocketmq_macros::RequestHeaderCodecV2;
+use serde::Deserialize;
+use serde::Serialize;
+
+#[derive(Clone, Debug, Serialize, Deserialize, Default, RequestHeaderCodecV2)]
+#[serde(rename_all = "camelCase")]
+pub struct ApplyBrokerIdRequestHeader {
+    pub cluster_name: CheetahString,
+    pub broker_name: CheetahString,
+    pub applied_broker_id: i64,
+    pub register_check_code: CheetahString,
+}
