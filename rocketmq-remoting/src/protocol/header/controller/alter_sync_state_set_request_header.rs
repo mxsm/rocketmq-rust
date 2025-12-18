086df8450605b2a3ac39f30edbac6ecabfa1d2dd
WaterWhisperer <waterwhisperer24@qq.com>
2025-12-18 18:07:26 +0800

    [ISSUE #4772]🧪Add test case for AlterSyncStateSetRequestHeader

--- rocketmq-remoting/src/protocol/header/controller/alter_sync_state_set_request_header.rs
+++ rocketmq-remoting/src/protocol/header/controller/alter_sync_state_set_request_header.rs
@@ -27,3 +27,80 @@ pub struct AlterSyncStateSetRequestHeader {
     pub master_broker_id: i64,
     pub master_epoch: i32,
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::*;
+    use crate::protocol::command_custom_header::CommandCustomHeader;
+    use crate::protocol::command_custom_header::FromMap;
+
+    #[test]
+    fn alter_sync_state_set_request_header_serializes_correctly() {
+        let header = AlterSyncStateSetRequestHeader {
+            broker_name: CheetahString::from_static_str("test_broker"),
+            master_broker_id: 1234567890,
+            master_epoch: 5,
+        };
+        let map = header.to_map().unwrap();
+        assert_eq!(
+            map.get(&CheetahString::from_static_str("brokerName"))
+                .unwrap(),
+            "test_broker"
+        );
+        assert_eq!(
+            map.get(&CheetahString::from_static_str("masterBrokerId"))
+                .unwrap(),
+            "1234567890"
+        );
+        assert_eq!(
+            map.get(&CheetahString::from_static_str("masterEpoch"))
+                .unwrap(),
+            "5"
+        );
+    }
+
+    #[test]
+    fn alter_sync_state_set_request_header_deserializes_correctly() {
+        let mut map = HashMap::new();
+        map.insert(
+            CheetahString::from_static_str("brokerName"),
+            CheetahString::from_static_str("test_broker"),
+        );
+        map.insert(
+            CheetahString::from_static_str("masterBrokerId"),
+            CheetahString::from_static_str("1234567890"),
+        );
+        map.insert(
+            CheetahString::from_static_str("masterEpoch"),
+            CheetahString::from_static_str("5"),
+        );
+
+        let header = <AlterSyncStateSetRequestHeader as FromMap>::from(&map).unwrap();
+        assert_eq!(header.broker_name, "test_broker");
+        assert_eq!(header.master_broker_id, 1234567890);
+        assert_eq!(header.master_epoch, 5);
+    }
+
+    #[test]
+    fn alter_sync_state_set_request_header_default() {
+        let header = AlterSyncStateSetRequestHeader::default();
+        assert_eq!(header.broker_name, "");
+        assert_eq!(header.master_broker_id, 0);
+        assert_eq!(header.master_epoch, 0);
+    }
+
+    #[test]
+    fn alter_sync_state_set_request_header_clone() {
+        let header = AlterSyncStateSetRequestHeader {
+            broker_name: CheetahString::from_static_str("test_broker"),
+            master_broker_id: 1234567890,
+            master_epoch: 5,
+        };
+        let cloned = header.clone();
+        assert_eq!(header.broker_name, cloned.broker_name);
+        assert_eq!(header.master_broker_id, cloned.master_broker_id);
+        assert_eq!(header.master_epoch, cloned.master_epoch);
+    }
+}
