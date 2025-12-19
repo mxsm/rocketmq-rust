636b899d38b784046622dfc74c2b82e8f0abcb38
WaterWhisperer <waterwhisperer24@qq.com>
2025-12-19 10:25:46 +0800

    [ISSUE #4773]🧪Add test case for AlterSyncStateSetResponseHeader (#4787)

--- rocketmq-remoting/src/protocol/header/controller/alter_sync_state_set_response_header.rs
+++ rocketmq-remoting/src/protocol/header/controller/alter_sync_state_set_response_header.rs
@@ -24,3 +24,57 @@ use serde::Serialize;
 pub struct AlterSyncStateSetResponseHeader {
     pub new_sync_state_set_epoch: i32,
 }
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use cheetah_string::CheetahString;
+
+    use super::*;
+    use crate::protocol::command_custom_header::CommandCustomHeader;
+    use crate::protocol::command_custom_header::FromMap;
+
+    #[test]
+    fn alter_sync_state_set_response_header_serializes_correctly() {
+        let header = AlterSyncStateSetResponseHeader {
+            new_sync_state_set_epoch: 10,
+        };
+        let map = header.to_map().unwrap();
+        assert_eq!(
+            map.get(&CheetahString::from_static_str("newSyncStateSetEpoch"))
+                .unwrap(),
+            "10"
+        );
+    }
+
+    #[test]
+    fn alter_sync_state_set_response_header_deserializes_correctly() {
+        let mut map = HashMap::new();
+        map.insert(
+            CheetahString::from_static_str("newSyncStateSetEpoch"),
+            CheetahString::from_static_str("10"),
+        );
+
+        let header = <AlterSyncStateSetResponseHeader as FromMap>::from(&map).unwrap();
+        assert_eq!(header.new_sync_state_set_epoch, 10);
+    }
+
+    #[test]
+    fn alter_sync_state_set_response_header_default() {
+        let header = AlterSyncStateSetResponseHeader::default();
+        assert_eq!(header.new_sync_state_set_epoch, 0);
+    }
+
+    #[test]
+    fn alter_sync_state_set_response_header_clone() {
+        let header = AlterSyncStateSetResponseHeader {
+            new_sync_state_set_epoch: 10,
+        };
+        let cloned = header.clone();
+        assert_eq!(
+            header.new_sync_state_set_epoch,
+            cloned.new_sync_state_set_epoch
+        );
+    }
+}
