49bc99304ce6f7a9b7b400bbbc000c57ec1e3488
mxsm <mxsm@apache.org>
2025-12-19 03:32:04 +0000

    [ISSUE #4801]🚀Add ElectMasterResponseBody struct

--- rocketmq-remoting/src/protocol/body.rs
+++ rocketmq-remoting/src/protocol/body.rs
@@ -36,6 +36,7 @@ pub mod consume_message_directly_result;
 pub mod consume_queue_data;
 pub mod consume_status;
 pub mod consumer_offset_serialize_wrapper;
+pub mod elect_master_response_body;
 pub mod epoch_entry_cache;
 pub mod group_list;
 pub mod ha_client_runtime_info;
