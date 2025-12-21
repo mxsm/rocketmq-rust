2f2292e0c2be0363fb7b3c26fc608bf6b70e265c
WaterWhisperer <waterwhisperer24@qq.com>
2025-12-21 13:38:12 +0800

    [ISSUE #4803]🧪Add test case for ApplyBrokerIdResponseHeader struct

--- rocketmq-remoting/src/protocol/header/controller/apply_broker_id_response_header.rs
+++ rocketmq-remoting/src/protocol/header/controller/apply_broker_id_response_header.rs
@@ -17,9 +17,80 @@
 
 use cheetah_string::CheetahString;
 use rocketmq_macros::RequestHeaderCodecV2;
+use serde::Deserialize;
+use serde::Serialize;
 
-#[derive(Clone, Debug, serde::Serialize, serde::Deserialize, RequestHeaderCodecV2)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default, RequestHeaderCodecV2)]
+#[serde(rename_all = "camelCase")]
 pub struct ApplyBrokerIdResponseHeader {
     pub cluster_name: Option<CheetahString>,
     pub broker_name: Option<CheetahString>,
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::*;
+    use crate::protocol::command_custom_header::CommandCustomHeader;
+    use crate::protocol::command_custom_header::FromMap;
+
+    #[test]
+    fn apply_broker_id_response_header_serializes_correctly() {
+        let header = ApplyBrokerIdResponseHeader {
+            cluster_name: Some(CheetahString::from_static_str("test_cluster")),
+            broker_name: Some(CheetahString::from_static_str("test_broker")),
+        };
+        let map = header.to_map().unwrap();
+        assert_eq!(
+            map.get(&CheetahString::from_static_str("clusterName"))
+                .unwrap(),
+            "test_cluster"
+        );
+        assert_eq!(
+            map.get(&CheetahString::from_static_str("brokerName"))
+                .unwrap(),
+            "test_broker"
+        );
+    }
+
+    #[test]
+    fn apply_broker_id_response_header_deserializes_correctly() {
+        let mut map = HashMap::new();
+        map.insert(
+            CheetahString::from_static_str("clusterName"),
+            CheetahString::from_static_str("test_cluster"),
+        );
+        map.insert(
+            CheetahString::from_static_str("brokerName"),
+            CheetahString::from_static_str("test_broker"),
+        );
+        let header = <ApplyBrokerIdResponseHeader as FromMap>::from(&map).unwrap();
+        assert_eq!(
+            header.cluster_name,
+            Some(CheetahString::from_static_str("test_cluster"))
+        );
+        assert_eq!(
+            header.broker_name,
+            Some(CheetahString::from_static_str("test_broker"))
+        );
+    }
+
+    #[test]
+    fn apply_broker_id_response_header_default() {
+        let header = ApplyBrokerIdResponseHeader::default();
+        assert_eq!(header.cluster_name, None);
+        assert_eq!(header.broker_name, None);
+    }
+
+    #[test]
+    fn apply_broker_id_response_header_clone() {
+        let header = ApplyBrokerIdResponseHeader {
+            cluster_name: Some(CheetahString::from_static_str("test_cluster")),
+            broker_name: Some(CheetahString::from_static_str("test_broker")),
+        };
+        let cloned_header = header.clone();
+        assert_eq!(header.cluster_name, cloned_header.cluster_name);
+        assert_eq!(header.broker_name, cloned_header.broker_name);
+    }
 }
