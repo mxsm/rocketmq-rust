40d4faa789c42cb2e7e34367a65bfe2888244fa4
mxsm <mxsm@apache.org>
2025-12-21 18:34:45 +0800

    [ISSUE #4891]🚀Add event module with EventType enum for standardized event identification and handling

---
+++ rocketmq-controller/src/event/event_type.rs
@@ -0,0 +1,66 @@
+//  Licensed to the Apache Software Foundation (ASF) under one
+//  or more contributor license agreements.  See the NOTICE file
+//  distributed with this work for additional information
+//  regarding copyright ownership.  The ASF licenses this file
+//  to you under the Apache License, Version 2.0 (the
+//  "License"); you may not use this file except in compliance
+//  with the License.  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing,
+//  software distributed under the License is distributed on an
+//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+//  KIND, either express or implied.  See the License for the
+//  specific language governing permissions and limitations
+//  under the License.
+
+use std::fmt;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+#[repr(i16)]
+#[allow(clippy::enum_variant_names)]
+pub enum EventType {
+    AlterSyncStateSetEvent = 1,
+    ApplyBrokerIdEvent = 2,
+    ElectMasterEvent = 3,
+    ReadEvent = 4,
+    CleanBrokerDataEvent = 5,
+    UpdateBrokerAddressEvent = 6,
+}
+
+impl EventType {
+    pub fn from_id(id: i16) -> Option<Self> {
+        match id {
+            1 => Some(Self::AlterSyncStateSetEvent),
+            2 => Some(Self::ApplyBrokerIdEvent),
+            3 => Some(Self::ElectMasterEvent),
+            4 => Some(Self::ReadEvent),
+            5 => Some(Self::CleanBrokerDataEvent),
+            6 => Some(Self::UpdateBrokerAddressEvent),
+            _ => None,
+        }
+    }
+
+    #[inline]
+    pub fn id(self) -> i16 {
+        self as i16
+    }
+
+    pub fn name(self) -> &'static str {
+        match self {
+            Self::AlterSyncStateSetEvent => "AlterSyncStateSetEvent",
+            Self::ApplyBrokerIdEvent => "ApplyBrokerIdEvent",
+            Self::ElectMasterEvent => "ElectMasterEvent",
+            Self::ReadEvent => "ReadEvent",
+            Self::CleanBrokerDataEvent => "CleanBrokerDataEvent",
+            Self::UpdateBrokerAddressEvent => "UpdateBrokerAddressEvent",
+        }
+    }
+}
+
+impl fmt::Display for EventType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(self.name())
+    }
+}
