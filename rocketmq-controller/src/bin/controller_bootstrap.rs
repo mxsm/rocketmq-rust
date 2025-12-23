1c12a3ea315755a937ed0ca6b974271abb07e009
mxsm <mxsm@apache.org>
2025-12-23 22:29:43 +0800

    [ISSUE #4954]✨Enhance controller_bootstrap with logger initialization, version info, and improved startup output

--- rocketmq-controller/src/bin/controller_bootstrap.rs
+++ rocketmq-controller/src/bin/controller_bootstrap.rs
@@ -15,9 +15,29 @@
 //  specific language governing permissions and limitations
 //  under the License.
 
+use rocketmq_common::common::mq_version::CURRENT_VERSION;
+use rocketmq_common::EnvUtils::EnvUtils;
+use rocketmq_error::Result;
+use rocketmq_remoting::protocol::remoting_command;
+
 #[rocketmq_rust::main]
-pub async fn main() {
+pub async fn main() -> Result<()> {
+    rocketmq_common::log::init_logger_with_level(rocketmq_common::log::Level::INFO)?;
+    const LOGO: &str = r#"
+        ______           _        _  ___  ________       ______          _     _____             _             _ _
+        | ___ \         | |      | | |  \/  |  _  |      | ___ \        | |   /  __ \           | |           | | |
+        | |_/ /___   ___| | _____| |_| .  . | | | |______| |_/ /   _ ___| |_  | /  \/ ___  _ __ | |_ _ __ ___ | | | ___ _ __
+        |    // _ \ / __| |/ / _ \ __| |\/| | | | |______|    / | | / __| __| | |    / _ \| '_ \| __| '__/ _ \| | |/ _ \ '__|
+        | |\ \ (_) | (__|   <  __/ |_| |  | \ \/' /      | |\ \ |_| \__ \ |_  | \__/\ (_) | | | | |_| | | (_) | | |  __/ |
+        \_| \_\___/ \___|_|\_\___|\__\_|  |_/\_/\_\      \_| \_\__,_|___/\__|  \____/\___/|_| |_|\__|_|  \___/|_|_|\___|_|
+    "#;
+    println!("        {}", LOGO);
+    EnvUtils::put_property(
+        remoting_command::REMOTING_VERSION_KEY,
+        (CURRENT_VERSION as u32).to_string(),
+    );
     // Initialize the controller here
     println!("RocketMQ Controller is starting...");
     // Add your initialization code here
+    Ok(())
 }
