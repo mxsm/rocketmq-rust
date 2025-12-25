2ccb684bc47f46e67490eef9f55c3cfb36cb37a8
mxsm <mxsm@apache.org>
2025-12-25 01:43:49 +0000

    [ISSUE #4989]✨Bump tonic, prost, and related dependencies to 0.14; switch build to tonic-prost-build

--- rocketmq-controller/build.rs
+++ rocketmq-controller/build.rs
@@ -17,17 +17,15 @@
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto/");
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .compile_protos(
-            &[
-                "proto/raft.proto",
-                "proto/controller.proto",
-                "proto/openraft.proto",
-            ],
-            &["proto/"],
-        )?;
+
+    tonic_prost_build::configure().compile_protos(
+        &[
+            "proto/raft.proto",
+            "proto/controller.proto",
+            "proto/openraft.proto",
+        ],
+        &["proto/"],
+    )?;
 
     Ok(())
 }
