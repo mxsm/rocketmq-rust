385de7d41ea4e709ec08e5f64ea6de6aa09a03ed
mxsm <mxsm@apache.org>
2025-12-29 04:46:26 +0000

    [ISSUE #5115]🚀Add base authorization context abstraction; extend default authorization context with channel ID support and builder methods

--- rocketmq-auth/src/authorization/context.rs
+++ rocketmq-auth/src/authorization/context.rs
@@ -17,5 +17,6 @@
 
 pub mod authentication_context;
 pub mod base_authentication_context;
+pub mod base_authorization_context;
 pub mod default_authentication_context;
 pub mod default_authorization_context;
