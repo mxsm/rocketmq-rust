d65b3dc9f0548f980dc83306393c3e3a75e7c270
mxsm <mxsm@apache.org>
2025-12-31 00:05:46 +0800

    [ISSUE #5210]✨style: format update_topic_publish_info signatures to single line for consistency (#5211)

--- rocketmq-client/src/producer/producer_impl/default_mq_producer_impl.rs
+++ rocketmq-client/src/producer/producer_impl/default_mq_producer_impl.rs
@@ -1958,11 +1958,7 @@ impl MQProducerInner for DefaultMQProducerImpl {
         });
     }
 
-    fn update_topic_publish_info(
-        &mut self,
-        topic: impl Into<CheetahString>,
-        info: Option<TopicPublishInfo>,
-    ) {
+    fn update_topic_publish_info(&mut self, topic: impl Into<CheetahString>, info: Option<TopicPublishInfo>) {
         let topic = topic.into();
         if topic.is_empty() || info.is_none() {
             return;
