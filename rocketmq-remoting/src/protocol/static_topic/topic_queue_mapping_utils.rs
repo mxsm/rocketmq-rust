8a150d5e16bc111ebdda65d473fef02efb3e8693
mxsm <mxsm@apache.org>
2026-01-01 17:22:13 +0800

    [ISSUE #5309]✨Normalize license headers across source files (#5310)

--- rocketmq-remoting/src/protocol/static_topic/topic_queue_mapping_utils.rs
+++ rocketmq-remoting/src/protocol/static_topic/topic_queue_mapping_utils.rs
@@ -5,22 +5,19 @@ use std::collections::HashSet;
 use cheetah_string::CheetahString;
 use rand::seq::SliceRandom;
 use rocketmq_common::common::config::TopicConfig;
-//  Licensed to the Apache Software Foundation (ASF) under one
-//  or more contributor license agreements.  See the NOTICE file
-//  distributed with this work for additional information
-//  regarding copyright ownership.  The ASF licenses this file
-//  to you under the Apache License, Version 2.0 (the
-//  "License"); you may not use this file except in compliance
-//  with the License.  You may obtain a copy of the License at
+// Copyright 2025-2026 The RocketMQ Rust Authors
 //
-//    http://www.apache.org/licenses/LICENSE-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Unless required by applicable law or agreed to in writing,
-//  software distributed under the License is distributed on an
-//  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-//  KIND, either express or implied.  See the License for the
-//  specific language governing permissions and limitations
-//  under the License.
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 use rocketmq_common::common::mix_all;
 use rocketmq_common::EnvUtils::EnvUtils;
 use rocketmq_common::FileUtils::string_to_file;
