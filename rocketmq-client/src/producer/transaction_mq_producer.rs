1785d9ae20b5110396db193953bf83f9b4c05240
onenewcode <lovestudy@qq.com>
2026-01-04 09:25:59 +0800

    [ISSUE #5415]Add error handlingor uninitialized TransactionMQProducer in send_message_in_transaction (#5418)

--- rocketmq-client/src/producer/transaction_mq_producer.rs
+++ rocketmq-client/src/producer/transaction_mq_producer.rs
@@ -289,7 +289,9 @@ impl MQProducer for TransactionMQProducer {
         self.default_producer
             .default_mqproducer_impl
             .as_mut()
-            .unwrap()
+            .ok_or(rocketmq_error::RocketMQError::not_initialized(
+                "DefaultMQProducerImpl is not initialized",
+            ))?
             .send_message_in_transaction(msg, arg.map(|x| Box::new(x) as Box<dyn Any + Sync + Send>))
             .await
     }
