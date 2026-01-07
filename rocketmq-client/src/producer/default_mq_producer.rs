845ee020dbc098d814bcaf676fedac0aad864b0c
WaterWhisperer <waterwhisperer24@qq.com>
2026-01-07 09:54:41 +0800

    [ISSUE #5487]✨Add error handlingor uninitialized DefaultMQProducer in send_with_selector_timeout (#5501)

--- rocketmq-client/src/producer/default_mq_producer.rs
+++ rocketmq-client/src/producer/default_mq_producer.rs
@@ -850,7 +850,7 @@ impl MQProducer for DefaultMQProducer {
         msg.set_topic(self.with_namespace(msg.get_topic()));
         self.default_mqproducer_impl
             .as_mut()
-            .unwrap()
+            .ok_or(RocketMQError::not_initialized("DefaultMQProducerImpl not initialized"))?
             .send_with_selector_timeout(msg, Arc::new(selector), arg, timeout)
             .await
     }
