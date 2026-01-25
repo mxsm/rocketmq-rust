5af82172c54677828fb5df64813a6d95c193a907
mxsm <mxsm@apache.org>
2026-01-25 18:43:24 +0800

    [ISSUE #5893]📝Enhance layout and styling with new contributing section, improved feature animations, and updated resource links (#5894)

--- rocketmq-website/src/theme/Layout.tsx
+++ rocketmq-website/src/theme/Layout.tsx
@@ -1,16 +1,9 @@
 /**
- * Layout wrapper to inject DevWarningBanner
- * Extends the default Docusaurus layout
+ * Layout wrapper to extend the default Docusaurus layout
  */
 
 import Layout from '@theme-original/Layout';
-import DevWarningBanner from '@site/src/components/DevWarningBanner';
 
 export default function LayoutWrapper(props: any): React.JSX.Element {
-  return (
-    <>
-      <DevWarningBanner />
-      <Layout {...props} />
-    </>
-  );
+  return <Layout {...props} />;
 }
