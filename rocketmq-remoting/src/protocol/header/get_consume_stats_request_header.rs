084e18f772dab0edc0027ee3d99605968405cebe
Aleksey <heleveins@gmail.com>
2026-02-17 11:58:10 +0100

    test: add unit test for GetConsumeStatsRequestHeader

--- rocketmq-remoting/src/protocol/header/get_consume_stats_request_header.rs
+++ rocketmq-remoting/src/protocol/header/get_consume_stats_request_header.rs
@@ -30,9 +30,6 @@ pub struct GetConsumeStatsRequestHeader {
 }
 
 impl GetConsumeStatsRequestHeader {
-    // pub const CONSUMER_GROUP: &'static str = "consumerGroup";
-    // pub const TOPIC: &'static str = "topic";
-
     pub fn get_consumer_group(&self) -> &CheetahString {
         &self.consumer_group
     }
@@ -45,5 +42,59 @@ impl GetConsumeStatsRequestHeader {
     }
     pub fn set_topic(&mut self, topic: CheetahString) {
         self.topic = topic;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_consume_stats_request_header() {
+        let mut header = GetConsumeStatsRequestHeader {
+            consumer_group: CheetahString::from("testGroup"),
+            topic: CheetahString::from("testTopic"),
+            topic_request_header: None,
+        };
+
+        assert_eq!(header.get_consumer_group(), "testGroup");
+        assert_eq!(header.get_topic(), "testTopic");
+
+        header.set_consumer_group(CheetahString::from("newGroup"));
+        header.set_topic(CheetahString::from("newTopic"));
+
+        assert_eq!(header.get_consumer_group(), "newGroup");
+        assert_eq!(header.get_topic(), "newTopic");
+    }
+    #[test]
+    fn get_consume_stats_request_header_serde() {
+        let header = GetConsumeStatsRequestHeader {
+            consumer_group: CheetahString::from("testGroup"),
+            topic: CheetahString::from("testTopic"),
+            topic_request_header: None,
+        };
+
+        let json = serde_json::to_string(&header).unwrap();
+        println!("Serialized JSON: {}", json);
+
+        let deserialized: GetConsumeStatsRequestHeader = serde_json::from_str(&json).unwrap();
+        assert_eq!(deserialized.get_consumer_group(), "testGroup");
+        assert_eq!(deserialized.get_topic(), "testTopic");
+    }
+
+    #[test]
+    fn get_consume_stats_request_header_deserialize_with_extra_fields() {
+        let json = r#"
+        {
+            "consumerGroup": "testGroup",
+            "topic": "testTopic",
+            "extraField1": "extraValue1",
+            "extraField2": "extraValue2"
+        }
+        "#;
+
+        let deserialized: GetConsumeStatsRequestHeader = serde_json::from_str(json).unwrap();
+        assert_eq!(deserialized.get_consumer_group(), "testGroup");
+        assert_eq!(deserialized.get_topic(), "testTopic");
     }
 }
