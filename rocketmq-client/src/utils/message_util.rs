b935fd52c392673aa783ded4cf4a85d2910d604f
mxsm <mxsm@apache.org>
2026-03-03 21:26:36 +0800

    [ISSUE #6683]✨Add benchmarks for MessageUtil and optimize property handling in create_reply_message

--- rocketmq-client/src/utils/message_util.rs
+++ rocketmq-client/src/utils/message_util.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::sync::LazyLock;
+
 use bytes::Bytes;
 use cheetah_string::CheetahString;
 use rocketmq_common::common::message::message_single::Message;
@@ -21,66 +23,79 @@ use rocketmq_common::MessageAccessor::MessageAccessor;
 
 use crate::common::client_error_code::ClientErrorCode;
 
+// Cached static strings to avoid repeated allocations
+static PROPERTY_CLUSTER: LazyLock<CheetahString> =
+    LazyLock::new(|| CheetahString::from_static_str(MessageConst::PROPERTY_CLUSTER));
+static PROPERTY_MESSAGE_TYPE: LazyLock<CheetahString> =
+    LazyLock::new(|| CheetahString::from_static_str(MessageConst::PROPERTY_MESSAGE_TYPE));
+static REPLY_MESSAGE_FLAG: LazyLock<CheetahString> =
+    LazyLock::new(|| CheetahString::from_static_str(mix_all::REPLY_MESSAGE_FLAG));
+static PROPERTY_MESSAGE_REPLY_TO_CLIENT: LazyLock<CheetahString> =
+    LazyLock::new(|| CheetahString::from_static_str(MessageConst::PROPERTY_MESSAGE_REPLY_TO_CLIENT));
+static PROPERTY_CORRELATION_ID: LazyLock<CheetahString> =
+    LazyLock::new(|| CheetahString::from_static_str(MessageConst::PROPERTY_CORRELATION_ID));
+static PROPERTY_MESSAGE_TTL: LazyLock<CheetahString> =
+    LazyLock::new(|| CheetahString::from_static_str(MessageConst::PROPERTY_MESSAGE_TTL));
+
 pub struct MessageUtil;
 
 impl MessageUtil {
     pub fn create_reply_message(request_message: &Message, body: &[u8]) -> rocketmq_error::RocketMQResult<Message> {
-        let mut reply_message = Message::default();
-        let cluster = request_message.property(&CheetahString::from_static_str(MessageConst::PROPERTY_CLUSTER));
-        if let Some(cluster) = cluster {
-            reply_message.set_body(Some(Bytes::copy_from_slice(body)));
-            let reply_topic = mix_all::get_retry_topic(cluster);
-            reply_message.set_topic(CheetahString::from_string(reply_topic));
-            MessageAccessor::put_property(
-                &mut reply_message,
-                CheetahString::from_static_str(MessageConst::PROPERTY_MESSAGE_TYPE),
-                CheetahString::from_static_str(mix_all::REPLY_MESSAGE_FLAG),
-            );
-            if let Some(reply_to) = request_message.property(&CheetahString::from_static_str(
-                MessageConst::PROPERTY_MESSAGE_REPLY_TO_CLIENT,
-            )) {
-                MessageAccessor::put_property(
-                    &mut reply_message,
-                    CheetahString::from_static_str(MessageConst::PROPERTY_MESSAGE_REPLY_TO_CLIENT),
-                    reply_to.into(),
-                );
-            }
-            if let Some(correlation_id) =
-                request_message.property(&CheetahString::from_static_str(MessageConst::PROPERTY_CORRELATION_ID))
-            {
-                MessageAccessor::put_property(
-                    &mut reply_message,
-                    CheetahString::from_static_str(MessageConst::PROPERTY_CORRELATION_ID),
-                    correlation_id.into(),
-                );
-            }
-            if let Some(ttl) =
-                request_message.property(&CheetahString::from_static_str(MessageConst::PROPERTY_MESSAGE_TTL))
-            {
-                MessageAccessor::put_property(
-                    &mut reply_message,
-                    CheetahString::from_static_str(MessageConst::PROPERTY_MESSAGE_TTL),
-                    ttl.into(),
-                );
-            }
-            Ok(reply_message)
-        } else {
-            let error = mq_client_err!(
+        // Early return: check required cluster property
+        let Some(cluster) = request_message.property(&PROPERTY_CLUSTER) else {
+            return Err(mq_client_err!(
                 ClientErrorCode::CREATE_REPLY_MESSAGE_EXCEPTION,
                 format!(
                     "create reply message fail, requestMessage error, property[{}] is null.",
                     MessageConst::PROPERTY_CLUSTER
                 )
+            ));
+        };
+
+        let mut reply_message = Message::default();
+        reply_message.set_body(Some(Bytes::copy_from_slice(body)));
+
+        let reply_topic = mix_all::get_retry_topic(cluster);
+        reply_message.set_topic(CheetahString::from_string(reply_topic));
+
+        // Set message type using cached static string
+        MessageAccessor::put_property(
+            &mut reply_message,
+            PROPERTY_MESSAGE_TYPE.clone(),
+            REPLY_MESSAGE_FLAG.clone(),
+        );
+
+        // Copy optional properties if present
+        if let Some(reply_to) = request_message.property(&PROPERTY_MESSAGE_REPLY_TO_CLIENT) {
+            MessageAccessor::put_property(
+                &mut reply_message,
+                PROPERTY_MESSAGE_REPLY_TO_CLIENT.clone(),
+                CheetahString::from_slice(reply_to),
             );
-            Err(error)
         }
+
+        if let Some(correlation_id) = request_message.property(&PROPERTY_CORRELATION_ID) {
+            MessageAccessor::put_property(
+                &mut reply_message,
+                PROPERTY_CORRELATION_ID.clone(),
+                CheetahString::from_slice(correlation_id),
+            );
+        }
+
+        if let Some(ttl) = request_message.property(&PROPERTY_MESSAGE_TTL) {
+            MessageAccessor::put_property(
+                &mut reply_message,
+                PROPERTY_MESSAGE_TTL.clone(),
+                CheetahString::from_slice(ttl),
+            );
+        }
+
+        Ok(reply_message)
     }
 
     pub fn get_reply_to_client(reply_message: &Message) -> Option<CheetahString> {
         reply_message
-            .property(&CheetahString::from_static_str(
-                MessageConst::PROPERTY_MESSAGE_REPLY_TO_CLIENT,
-            ))
-            .map(Into::into)
+            .property(&PROPERTY_MESSAGE_REPLY_TO_CLIENT)
+            .map(CheetahString::from_slice)
     }
 }
