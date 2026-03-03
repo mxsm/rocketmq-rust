a469401a041ac5b0cf7787cc32b0d88d74537d38
mxsm <mxsm@apache.org>
2026-03-03 15:34:03 +0800

    [ISSUE #6669]✨Add CheckForbiddenContext and CheckForbiddenHook traits for message validation

--- rocketmq-client/src/hook/check_forbidden_context.rs
+++ rocketmq-client/src/hook/check_forbidden_context.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::fmt;
+
 use cheetah_string::CheetahString;
 use rocketmq_common::common::message::message_queue::MessageQueue;
 use rocketmq_common::common::message::MessageTrait;
@@ -19,16 +21,142 @@ use rocketmq_common::common::message::MessageTrait;
 use crate::implementation::communication_mode::CommunicationMode;
 use crate::producer::send_result::SendResult;
 
+/// Context information for forbidden operation checks.
+///
+/// Contains all relevant information about a message send operation,
+/// allowing hooks to inspect and validate the operation before execution.
+///
+/// # Examples
+///
+/// ```ignore
+/// use rocketmq_client::hook::check_forbidden_context::CheckForbiddenContext;
+/// use cheetah_string::CheetahString;
+///
+/// let mut context = CheckForbiddenContext::default();
+/// context.group = Some(CheetahString::from("test_group"));
+/// context.broker_addr = Some(CheetahString::from("127.0.0.1:10911"));
+/// context.unit_mode = false;
+/// ```
 #[derive(Default)]
 pub struct CheckForbiddenContext<'a> {
+    /// Name server address
     pub name_srv_addr: Option<CheetahString>,
+    /// Producer or consumer group name
     pub group: Option<CheetahString>,
+    /// Message being sent (borrowed reference)
     pub message: Option<&'a dyn MessageTrait>,
+    /// Target message queue (borrowed reference)
     pub mq: Option<&'a MessageQueue>,
+    /// Broker address
     pub broker_addr: Option<CheetahString>,
+    /// Communication mode (sync, async, or oneway)
     pub communication_mode: Option<CommunicationMode>,
+    /// Send result (available after sending)
     pub send_result: Option<SendResult>,
-    pub exception: Option<Box<dyn std::error::Error>>,
-    pub arg: Option<Box<dyn std::any::Any>>,
+    /// Exception that occurred during sending
+    pub exception: Option<Box<dyn std::error::Error + Send + Sync>>,
+    /// Custom argument
+    pub arg: Option<Box<dyn std::any::Any + Send + Sync>>,
+    /// Whether unit mode is enabled
     pub unit_mode: bool,
+}
+
+impl<'a> CheckForbiddenContext<'a> {
+    /// Creates a new context.
+    #[inline]
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Sets the name server address.
+    #[inline]
+    pub fn with_name_srv_addr(mut self, addr: impl Into<CheetahString>) -> Self {
+        self.name_srv_addr = Some(addr.into());
+        self
+    }
+
+    /// Sets the group name.
+    #[inline]
+    pub fn with_group(mut self, group: impl Into<CheetahString>) -> Self {
+        self.group = Some(group.into());
+        self
+    }
+
+    /// Sets the broker address.
+    #[inline]
+    pub fn with_broker_addr(mut self, addr: impl Into<CheetahString>) -> Self {
+        self.broker_addr = Some(addr.into());
+        self
+    }
+
+    /// Sets the communication mode.
+    #[inline]
+    pub fn with_communication_mode(mut self, mode: CommunicationMode) -> Self {
+        self.communication_mode = Some(mode);
+        self
+    }
+
+    /// Sets the unit mode flag.
+    #[inline]
+    pub fn with_unit_mode(mut self, unit_mode: bool) -> Self {
+        self.unit_mode = unit_mode;
+        self
+    }
+}
+
+impl<'a> fmt::Display for CheckForbiddenContext<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "CheckForbiddenContext {{ ")?;
+
+        if let Some(ref addr) = self.name_srv_addr {
+            write!(f, "name_srv_addr: {}, ", addr)?;
+        }
+
+        if let Some(ref group) = self.group {
+            write!(f, "group: {}, ", group)?;
+        }
+
+        if self.message.is_some() {
+            write!(f, "message: Some(_), ")?;
+        }
+
+        if let Some(ref mq) = self.mq {
+            write!(f, "mq: {}, ", mq)?;
+        }
+
+        if let Some(ref addr) = self.broker_addr {
+            write!(f, "broker_addr: {}, ", addr)?;
+        }
+
+        if let Some(ref mode) = self.communication_mode {
+            write!(f, "communication_mode: {:?}, ", mode)?;
+        }
+
+        if self.send_result.is_some() {
+            write!(f, "send_result: Some(_), ")?;
+        }
+
+        if self.exception.is_some() {
+            write!(f, "exception: Some(_), ")?;
+        }
+
+        write!(f, "unit_mode: {} }}", self.unit_mode)
+    }
+}
+
+impl<'a> fmt::Debug for CheckForbiddenContext<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("CheckForbiddenContext")
+            .field("name_srv_addr", &self.name_srv_addr)
+            .field("group", &self.group)
+            .field("message", &self.message.as_ref().map(|_| "Some(_)"))
+            .field("mq", &self.mq)
+            .field("broker_addr", &self.broker_addr)
+            .field("communication_mode", &self.communication_mode)
+            .field("send_result", &self.send_result.as_ref().map(|_| "Some(_)"))
+            .field("exception", &self.exception.as_ref().map(|_| "Some(_)"))
+            .field("arg", &self.arg.as_ref().map(|_| "Some(_)"))
+            .field("unit_mode", &self.unit_mode)
+            .finish()
+    }
 }
--- rocketmq-client/src/hook/check_forbidden_hook.rs
+++ rocketmq-client/src/hook/check_forbidden_hook.rs
@@ -14,8 +14,49 @@
 
 use crate::hook::check_forbidden_context::CheckForbiddenContext;
 
+/// Hook for checking forbidden message sending operations.
+///
+/// This trait allows custom validation logic to be executed before messages are sent.
+/// Implementations can inspect the message context and reject operations that violate
+/// business rules or security policies.
+///
+/// All implementations must be `Send + Sync` to support concurrent message sending
+/// in multi-threaded producer environments.
+///
+/// # Examples
+///
+/// ```ignore
+/// use rocketmq_client::hook::check_forbidden_hook::CheckForbiddenHook;
+/// use rocketmq_client::hook::check_forbidden_context::CheckForbiddenContext;
+/// use rocketmq_error::RocketMQResult;
+///
+/// struct RegionValidator;
+///
+/// impl CheckForbiddenHook for RegionValidator {
+///     fn hook_name(&self) -> &str {
+///         "RegionValidator"
+///     }
+///
+///     fn check_forbidden(&self, context: &CheckForbiddenContext) -> RocketMQResult<()> {
+///         if let Some(broker_addr) = &context.broker_addr {
+///             if broker_addr.contains("forbidden-region") {
+///                 return Err("Cannot send to forbidden region".into());
+///             }
+///         }
+///         Ok(())
+///     }
+/// }
+/// ```
 pub trait CheckForbiddenHook: Send + Sync {
+    /// Returns the unique name of this hook.
+    ///
+    /// The name is used for logging and diagnostic purposes.
     fn hook_name(&self) -> &str;
 
+    /// Validates whether a message send operation should be allowed.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the operation should be rejected based on the context.
     fn check_forbidden(&self, context: &CheckForbiddenContext<'_>) -> rocketmq_error::RocketMQResult<()>;
 }
