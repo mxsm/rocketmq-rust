ee9eea708743a404a2993fb06f272c0653257705
Aleksey <heleveins@gmail.com>
2026-03-14 11:29:58 +0100

    [ISSUE #6177]🧪Add unit tests for ListAclRequestHeader (#6724)

--- rocketmq-remoting/src/protocol/header/list_acl_request_header.rs
+++ rocketmq-remoting/src/protocol/header/list_acl_request_header.rs
@@ -23,3 +23,56 @@ pub struct ListAclRequestHeader {
     pub subject_filter: CheetahString,
     pub resource_filter: CheetahString,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cheetah_string::CheetahString;
+
+    #[test]
+    fn list_acl_request_header_default() {
+        let header: ListAclRequestHeader = Default::default();
+        assert_eq!(header.subject_filter, CheetahString::default());
+        assert_eq!(header.resource_filter, CheetahString::default());
+    }
+
+    #[test]
+    fn list_acl_request_header_serialize() {
+        let header = ListAclRequestHeader {
+            subject_filter: CheetahString::from("subjectFilterValue"),
+            resource_filter: CheetahString::from("resourceFilterValue"),
+        };
+
+        let json_str = serde_json::to_string(&header).unwrap();
+
+        // Verify the serialized JSON contains expected fields with camelCase naming
+        assert!(json_str.contains("\"subjectFilter\":\"subjectFilterValue\""));
+        assert!(json_str.contains("\"resourceFilter\":\"resourceFilterValue\""));
+    }
+
+    #[test]
+    fn list_acl_request_header_deserialize() {
+        let json_str = r#"{
+            "subjectFilter": "subjectFilterValue",
+            "resourceFilter": "resourceFilterValue"
+        }"#;
+
+        let header: ListAclRequestHeader = serde_json::from_str(json_str).unwrap();
+
+        assert_eq!(header.subject_filter, CheetahString::from("subjectFilterValue"));
+        assert_eq!(header.resource_filter, CheetahString::from("resourceFilterValue"));
+    }
+
+    #[test]
+    fn list_acl_request_header_clone() {
+        let header = ListAclRequestHeader {
+            subject_filter: CheetahString::from("subjectFilterValue"),
+            resource_filter: CheetahString::from("resourceFilterValue"),
+        };
+
+        let cloned_header = header.clone();
+
+        assert_eq!(header.subject_filter, cloned_header.subject_filter);
+        assert_eq!(header.resource_filter, cloned_header.resource_filter);
+    }
+}
