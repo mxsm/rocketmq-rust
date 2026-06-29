c1b092a2d96849a0819c2da10c9c58e818f4995b
onenewcode <lovestudy@qq.com>
2026-06-29 14:20:37 +0800

    [ISSUE #7711]🐛Add recursion limit to grpc ingress test (#7712)

--- rocketmq-proxy/tests/grpc_ingress.rs
+++ rocketmq-proxy/tests/grpc_ingress.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![recursion_limit = "256"]
+
 use std::collections::HashMap;
 use std::fs;
 use std::net::SocketAddr;
