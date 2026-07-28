c2978a21d69bdc767ff08deab229a59af2d92609
mxsm <mxsm@apache.org>
2026-07-28 21:29:53 +0800

    feat(sre): register autonomy routes

--- rocketmq-sre/crates/rocketmq-sre-control-plane/src/autonomy.rs
+++ rocketmq-sre/crates/rocketmq-sre-control-plane/src/autonomy.rs
@@ -12,8 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod api;
 mod model;
 mod repository;
 mod service;
 
+pub(crate) use api::routes;
 pub(crate) use service::AutonomyService;
