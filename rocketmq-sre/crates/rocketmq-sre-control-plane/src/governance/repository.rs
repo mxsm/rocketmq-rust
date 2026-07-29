ef30dd114cc540f755fa8ea2093067a77db077ed
mxsm <mxsm@apache.org>
2026-07-29 12:28:11 +0800

    feat(sre): add governance registry repository

---
+++ rocketmq-sre/crates/rocketmq-sre-control-plane/src/governance/repository.rs
@@ -0,0 +1,30 @@
+// Copyright 2026 The RocketMQ Rust Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+mod registry;
+mod reporting;
+mod support;
+
+use sqlx::PgPool;
+
+#[derive(Clone)]
+pub(super) struct GovernanceRepository {
+    pub(super) pool: PgPool,
+}
+
+impl GovernanceRepository {
+    pub(super) fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+}
