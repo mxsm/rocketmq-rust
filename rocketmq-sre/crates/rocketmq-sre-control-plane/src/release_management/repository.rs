58137dd33288471ea68b1b666eb1f10eee32b6fb
mxsm <mxsm@apache.org>
2026-07-29 11:47:02 +0800

    feat(sre): register enterprise integration repository

--- rocketmq-sre/crates/rocketmq-sre-control-plane/src/release_management/repository.rs
+++ rocketmq-sre/crates/rocketmq-sre-control-plane/src/release_management/repository.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod enterprise_integration;
 mod integration;
 mod release;
 mod support;
