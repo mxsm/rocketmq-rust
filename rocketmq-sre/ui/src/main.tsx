932d42927b8105d69369049e8957019d1c3bc003
mxsm <mxsm@apache.org>
2026-07-29 13:40:29 +0800

    style(sre-ui): load enterprise operation styles

--- rocketmq-sre/ui/src/main.tsx
+++ rocketmq-sre/ui/src/main.tsx
@@ -9,6 +9,7 @@ import "./styles/tokens.css";
 import "./styles/app.css";
 import "./styles/autonomy-operations.css";
 import "./styles/change-management.css";
+import "./styles/enterprise-operations.css";
 import "./styles/model-governance.css";
 import "./styles/release-management.css";
 
