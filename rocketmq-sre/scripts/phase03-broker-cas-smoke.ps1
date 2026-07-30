6719e3771a97b785897cf21992e1d77d83504ce5
mxsm <mxsm@apache.org>
2026-07-31 00:53:47 +0800

    chore(sre): move phase03-broker-cas-smoke workspace data to D drive

--- rocketmq-sre/scripts/phase03-broker-cas-smoke.ps1
+++ rocketmq-sre/scripts/phase03-broker-cas-smoke.ps1
@@ -4,9 +4,9 @@
 [CmdletBinding()]
 param(
     [string]$DatabaseUrl = 'postgres://rocketmq_sre:rocketmq_sre@127.0.0.1:5432/rocketmq_sre',
-    [string]$CargoTargetDir = 'G:\rocketmq-sre-phase2-cargo-target',
-    [string]$CargoHome = 'G:\rocketmq-sre-phase1-cargo-home',
-    [string]$TemporaryRoot = 'G:\rocketmq-sre-phase2-temp',
+    [string]$CargoTargetDir = 'D:\BuildCache\rocketmq-sre-target',
+    [string]$CargoHome = 'D:\BuildCache\rocketmq-sre-cargo-home',
+    [string]$TemporaryRoot = 'D:\BuildCache\rocketmq-sre-temp',
     [ValidateRange(1024, 65535)]
     [int]$NameServerPort = 19876,
     [ValidateRange(1026, 65535)]
