82af21b59a783c43bc3452564d6f09fb42cecb65
mxsm <mxsm@apache.org>
2026-08-07 21:59:54 +0800

    [ISSUE #9068]✨Migrate inherited request headers to V3 (#9069)
    Fixes #9068

--- rocketmq-protocol/src/protocol/header/lite_subscription_ctl_request_header.rs
+++ rocketmq-protocol/src/protocol/header/lite_subscription_ctl_request_header.rs
@@ -12,16 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use rocketmq_macros::RequestHeaderCodecV2;
+use rocketmq_macros::RequestHeaderCodecV3;
 use serde::Deserialize;
 use serde::Serialize;
 
 use crate::rpc::rpc_request_header::RpcRequestHeader;
 
-#[derive(Clone, Debug, Serialize, Deserialize, Default, RequestHeaderCodecV2)]
+#[derive(Clone, Debug, Serialize, Deserialize, Default, RequestHeaderCodecV3)]
+#[header(
+    type_id = "rocketmq_protocol::protocol::header::lite_subscription_ctl_request_header::LiteSubscriptionCtlRequestHeader",
+    java_class = "org.apache.rocketmq.remoting.protocol.header.LiteSubscriptionCtlRequestHeader"
+)]
 #[serde(rename_all = "camelCase")]
 pub struct LiteSubscriptionCtlRequestHeader {
     #[serde(flatten)]
+    #[header(flatten, presence = "always")]
     pub rpc_request_header: Option<RpcRequestHeader>,
 }
 
