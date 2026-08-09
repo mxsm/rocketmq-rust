1e17be83e89a03c819de859d522f5ecd0b1bba9d
mxsm <mxsm@apache.org>
2026-08-09 13:54:57 +0800

    [ISSUE #9148]🐛Bound example shutdown future layout

--- rocketmq-client/examples/ordermessage/random_selector_producer.rs
+++ rocketmq-client/examples/ordermessage/random_selector_producer.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![recursion_limit = "256"]
-
 //! Example demonstrating the use of SelectMessageQueueByRandom for load-balanced message delivery.
 //!
 //! This example shows how to use random queue selection to distribute messages
