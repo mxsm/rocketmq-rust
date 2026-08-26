a7be7558972e5e16f6ff1da1e0e8ebe65e1b1d6d
mxsm <mxsm@apache.org>
2026-08-26 14:48:13 +0800

    [ISSUE #9759]✨Establish a curated V2 request-processing API boundary(#9760)

---
+++ rocketmq-transport/src/public_api_v2.rs
@@ -0,0 +1,20 @@
+// Copyright 2026 The RocketMQ Rust Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Explicitly approved source API for the 2.x release line.
+//!
+//! [`RequestDeadline`] is currently the only shared type. The 2.x request
+//! model is not yet publicly exposed.
+
+pub use crate::deadline::RequestDeadline;
