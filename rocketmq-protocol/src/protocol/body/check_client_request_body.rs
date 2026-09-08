aaed78082f501ad4d2e236920a53a1bd8e2cac8e
WaterWhisperer <waterwhisperer24@qq.com>
2026-09-08 14:59:25 +0800

    [ISSUE #10173]♻️Remove getter and setter restatement tests from protocol body and header structs

--- rocketmq-protocol/src/protocol/body/check_client_request_body.rs
+++ rocketmq-protocol/src/protocol/body/check_client_request_body.rs
@@ -74,32 +74,6 @@ mod tests {
     }
 
     #[test]
-    fn test_new_and_getters() {
-        let sub_data = create_mock_subscription();
-        let body = CheckClientRequestBody::new(
-            "some_client-123".to_string(),
-            "some_group-abc".to_string(),
-            sub_data.clone(),
-        );
-
-        assert_eq!(body.get_client_id(), "some_client-123");
-        assert_eq!(body.get_group(), "some_group-abc");
-        assert_eq!(body.get_subscription_data().topic, "test-topic");
-        assert!(body.namespace.is_none());
-    }
-
-    #[test]
-    fn test_setters() {
-        let mut body = CheckClientRequestBody::new("old_id".into(), "old_group".into(), create_mock_subscription());
-
-        body.set_client_id("new_id".into());
-        body.set_group("new_group".into());
-
-        assert_eq!(body.client_id, "new_id");
-        assert_eq!(body.group, "new_group");
-    }
-
-    #[test]
     fn test_serialization_camel_case() {
         let sub_data = create_mock_subscription();
         let body = CheckClientRequestBody::new("c1".into(), "g1".into(), sub_data);
